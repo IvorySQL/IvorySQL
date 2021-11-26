09c1386368cb4784a69e1bad7ac5d12443fb03ed
Huajianjin <m18628910446@163.com>
2021-11-26 10:15:00 +0800

    Compatible with oracle bin_to_num function

--- contrib/orafce/builtins.h
+++ contrib/orafce/builtins.h
@@ -79,6 +79,7 @@ extern PGDLLEXPORT Datum orafce_to_multi_byte(PG_FUNCTION_ARGS);
 extern PGDLLEXPORT Datum orafce_to_single_byte(PG_FUNCTION_ARGS);
 extern PGDLLEXPORT Datum orafce_unistr(PG_FUNCTION_ARGS);
 extern PGDLLEXPORT Datum orafce_to_varchar(PG_FUNCTION_ARGS);
+extern PGDLLEXPORT Datum orafce_bin_to_num(PG_FUNCTION_ARGS);
 
 /* from datefce.c */
 extern PGDLLEXPORT Datum next_day(PG_FUNCTION_ARGS);
