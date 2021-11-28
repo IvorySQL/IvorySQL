4cfea929db07e0ec1d7cf7ecc808cd07c931d8a8
Huajianjin <m18628910446@163.com>
2021-11-28 21:02:48 +0800

    Compatible with oracle to_timestamp and to_timestamp_tz function

--- contrib/orafce/sql/convertfunc.sql
+++ contrib/orafce/sql/convertfunc.sql
@@ -46,4 +46,33 @@ select oracle.hex_to_decimal('0x7fffffffffffffff');
 select oracle.hex_to_decimal('0x8fffffffffffffff');
 select oracle.hex_to_decimal(NULL);
 
+select oracle.to_timestamp_tz('2020-01-15');
+select oracle.to_timestamp_tz('2020-01-15'::text);
+select oracle.to_timestamp_tz('2019','yyyy');
+select oracle.to_timestamp_tz('2019-11','yyyy-mm');
+select oracle.to_timestamp_tz('2003/12/13 10:13:18 -8:00');
+select oracle.to_timestamp_tz('2003/12/13 10:13:18 +1:00');
+select oracle.to_timestamp_tz('2003/12/13 10:13:18 +7:00');
+select oracle.to_timestamp_tz('2019-11-25'::text,'yyyy-mm-dd HH24:MI:SS OF');
+select oracle.to_timestamp_tz('2019-11-25 13:38:00'::text,'yyyy-mm-dd HH24:MI:SS');
+select oracle.to_timestamp_tz('2019-11-29 14:17:00','YYYY-MM-DD HH24:MI:SS TZH:TZM');
+select oracle.to_timestamp_tz('2003/12/13 10:13:18 -8:00', 'YYYY/MM/DD HH:MI:SS TZH:TZM');
+select oracle.to_timestamp_tz('2019/12/13 10:13:18 -2:00', 'YYYY/MM/DD HH:MI:SS TZH:TZM');
+select oracle.to_timestamp_tz('2019/12/13 10:13:18 +5:00', 'YYYY/MM/DD HH:MI:SS TZH:TZM');
+select oracle.to_timestamp_tz(NULL);
+
+select oracle.to_timestamp(-12124579);
+select oracle.to_timestamp(-1212457999999999999999999);
+select oracle.to_timestamp(12121212.55::float);
+select oracle.to_timestamp(1212121212.55);
+select oracle.to_timestamp(1212121212.55::real);
+select oracle.to_timestamp(NULL);
+select oracle.to_timestamp(1212121212.55::numeric);
+select oracle.to_timestamp('2019-11-29 14:17:00','YYYY-MM-DD HH24:MI:SS TZH:TZM');
+select oracle.to_timestamp('2020/03/03 10:13:18 -8:00', 'YYYY/MM/DD HH:MI:SS TZH:TZM');
+select oracle.to_timestamp('2020/03/03 10:13:18 -2:00', 'YYYY/MM/DD HH:MI:SS TZH:TZM');
+select oracle.to_timestamp('2020/03/03 10:13:18 +5:00', 'YYYY/MM/DD HH:MI:SS TZH:TZM');
+select oracle.to_timestamp(NULL,NULL);
+select oracle.to_timestamp(1212457898999999999999);
+
 set compatible_mode to default;
