8e0c39d1e10f09331390cc9b45ab9fd3f8dec7f7
aobing <aobing@highgo.com>
2021-12-06 01:34:16 -0500

    merge syntax conflict

--- src/backend/commands/functioncmds.c
+++ src/backend/commands/functioncmds.c
@@ -2421,11 +2421,4 @@ CallStmtResultDesc(CallStmt *stmt)
 
 	return tupdesc;
 }
-iled for procedure %u", fexpr->funcid);
 
-	tupdesc = build_function_result_tupdesc_t(tuple);
-
-	ReleaseSysCache(tuple);
-
-	return tupdesc;
-}
