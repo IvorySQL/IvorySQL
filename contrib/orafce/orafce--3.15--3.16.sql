9b7cf1b7b91731d42a45851acea2009a9d8cd073
lanke <lanke@highgo.com>
2022-01-06 17:01:24 +0800

    Remove redundant sql files and upgrade the orafce version to 3.18

--- contrib/orafce/orafce--3.15--3.16.sql
+++
@@ -1,5 +0,0 @@
-CREATE FUNCTION dbms_utility.get_time()
-RETURNS int
-AS 'MODULE_PATHNAME','dbms_utility_get_time'
-LANGUAGE C VOLATILE;
-COMMENT ON FUNCTION dbms_utility.get_time() IS 'Returns the number of hundredths of seconds that have elapsed since point in time';
