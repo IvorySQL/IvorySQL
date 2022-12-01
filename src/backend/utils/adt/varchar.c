acf95e903cd6e4050ef03224d9df43b8f20378df
Jeff Davis <jdavis@postgresql.org>
2022-12-01 11:26:32 -0800

    Fix memory leak for hashing with nondeterministic collations.
    Backpatch through 12, where nondeterministic collations were
    introduced (5e1963fb76).
    
    Backpatch-through: 12

--- src/backend/utils/adt/varchar.c
+++ src/backend/utils/adt/varchar.c
@@ -1123,6 +1123,7 @@ hashbpchar(PG_FUNCTION_ARGS)
 			buf = palloc(bsize);
 			ucol_getSortKey(mylocale->info.icu.ucol,
 							uchar, ulen, buf, bsize);
+			pfree(uchar);
 
 			result = hash_any(buf, bsize);
 
@@ -1184,6 +1185,7 @@ hashbpcharextended(PG_FUNCTION_ARGS)
 			buf = palloc(bsize);
 			ucol_getSortKey(mylocale->info.icu.ucol,
 							uchar, ulen, buf, bsize);
+			pfree(uchar);
 
 			result = hash_any_extended(buf, bsize, PG_GETARG_INT64(1));
 
