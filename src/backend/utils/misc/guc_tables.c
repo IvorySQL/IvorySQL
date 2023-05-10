df3c32e933e00a3fa9b316df26afb1ca88a94bbe
Michael Paquier <michael@paquier.xyz>
2023-05-10 11:24:30 +0900

    Fix assertion failure when updating stats_fetch_consistency in a transaction
    An update of the GUC stats_fetch_consistency in a transaction would be
    able to trigger an assertion when doing cache->snapshot.  In this case,
    when retrieving a pgstat entry after the switch, a new snapshot would be
    rebuilt, confusing pgstat_build_snapshot() because a snapshot is already
    cached with an unexpected mode ("cache").
    
    In order to fix this problem, this commit adds a flag to force a
    snapshot clear each time this GUC is changed.  Some tests are added to
    check, while on it.
    
    Some optimizations in avoiding the snapshot clear should be possible
    depending on what is cached and the current GUC value, I guess, but this
    solution is simple, and ensures that the state of the cache is updated
    each time a new pgstat entry is fetched, hence being consistent with the
    level wanted by the client that has set the GUC.
    
    Note that cache->none and snapshot->none would not cause issues, as
    fetching a pgstat entry would be retrieved from shared memory on the
    second attempt, however a snapshot would still be cached.  Similarly,
    none->snapshot and none->cache would build a new snapshot on the second
    fetch attempt.  Finally, snapshot->cache would cache a new snapshot on
    the second attempt.
    
    Reported-by: Alexander Lakhin
    Author: Kyotaro Horiguchi
    Discussion: https://postgr.es/m/17804-2a118cd046f2d0e5@postgresql.org
    backpatch-through: 15

--- src/backend/utils/misc/guc_tables.c
+++ src/backend/utils/misc/guc_tables.c
@@ -5006,7 +5006,7 @@ struct config_enum ConfigureNamesEnum[] =
 		},
 		&pgstat_fetch_consistency,
 		PGSTAT_FETCH_CONSISTENCY_CACHE, stats_fetch_consistency,
-		NULL, NULL, NULL
+		NULL, assign_stats_fetch_consistency, NULL
 	},
 
 	{
