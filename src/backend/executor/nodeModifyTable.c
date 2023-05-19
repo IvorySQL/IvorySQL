54089f13b2716b5988c9576282f219f7bd4c1011
Tom Lane <tgl@sss.pgh.pa.us>
2023-05-19 14:26:34 -0400

    Fix misbehavior of EvalPlanQual checks with multiple result relations.
    The idea of EvalPlanQual is that we replace the query's scan of the
    result relation with a single injected tuple, and see if we get a
    tuple out, thereby implying that the injected tuple still passes the
    query quals.  (In join cases, other relations in the query are still
    scanned normally.)  This logic was not updated when commit 86dc90056
    made it possible for a single DML query plan to have multiple result
    relations, when the query target relation has inheritance or partition
    children.  We replaced the output for the current result relation
    successfully, but other result relations were still scanned normally;
    thus, if any other result relation contained a tuple satisfying the
    quals, we'd think the EPQ check passed, even if it did not pass for
    the injected tuple itself.  This would lead to update or delete
    actions getting performed when they should have been skipped due to
    a conflicting concurrent update in READ COMMITTED isolation mode.
    
    Fix by blocking all sibling result relations from emitting tuples
    during an EvalPlanQual recheck.  In the back branches, the fix is
    complicated a bit by the need to not change the size of struct
    EPQState (else we'd have ABI-breaking changes in offsets in
    struct ModifyTableState).  Like the back-patches of 3f7836ff6
    and 4b3e37993, add a separately palloc'd struct to avoid that.
    The logic is the same as in HEAD otherwise.
    
    This is only a live bug back to v14 where 86dc90056 came in.
    However, I chose to back-patch the test cases further, on the
    grounds that this whole area is none too well tested.  I skipped
    doing so in v11 though because none of the test applied cleanly,
    and it didn't quite seem worth extra work for a branch with only
    six months to live.
    
    Per report from Ante Krešić (via Aleksander Alekseev)
    
    Discussion: https://postgr.es/m/CAJ7c6TMBTN3rcz4=AjYhLPD_w3FFT0Wq_C15jxCDn8U4tZnH1g@mail.gmail.com

--- src/backend/executor/nodeModifyTable.c
+++ src/backend/executor/nodeModifyTable.c
@@ -3980,7 +3980,8 @@ ExecInitModifyTable(ModifyTable *node, EState *estate, int eflags)
 	}
 
 	/* set up epqstate with dummy subplan data for the moment */
-	EvalPlanQualInit(&mtstate->mt_epqstate, estate, NULL, NIL, node->epqParam);
+	EvalPlanQualInitExt(&mtstate->mt_epqstate, estate, NULL, NIL,
+						node->epqParam, node->resultRelations);
 	mtstate->fireBSTriggers = true;
 
 	/*
