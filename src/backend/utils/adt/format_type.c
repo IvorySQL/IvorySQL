e98b4b424f94f599862c4fb7beb7c2a31e546fb4
wangjie <116478190+wangjie-star@users.noreply.github.com>
2023-06-05 14:34:28 +0800

    Revert "IvorySQL: fix bug - Some new data types have double quotes"
    This reverts commit 2c31038abfa7aaaa3117808302af17b16fb41e46.

--- src/backend/utils/adt/format_type.c
+++ src/backend/utils/adt/format_type.c
@@ -366,14 +366,6 @@ format_type_extended(Oid type_oid, int32 typemod, bits16 flags)
 			else
 				buf = pstrdup("number");
 			break;
-
-		case BINARY_FLOATOID:
-			buf = pstrdup("binary_float");
-			break;
-		case BINARY_DOUBLEOID:
-			buf = pstrdup("binary_double");
-			break;
-
 		/* IvorySQL:END - datatype */
 	}
 
