9c6dfba0e9a506e8e607cd3bf1e900433ab9d6a1
DutMsn <1371392902@qq.com>
2023-06-13 22:15:11 +0800

    IvorySQL: merge patch -- refer to 9df8f90
    This patch is to fix the bug of previously submmit

--- src/oracle_test/regress/sql/join.sql
+++ src/oracle_test/regress/sql/join.sql
@@ -441,6 +441,7 @@ select a.f1, b.f1, t.thousand, t.tenthous from
   (select sum(f1) as f1 from int4_tbl i4b) b
 where b.f1 = t.thousand and a.f1 = b.f1 and (a.f1+b.f1+999) = t.tenthous;
 
+--
 -- checks for correct handling of quals in multiway outer joins
 --
 explain (costs off)
