652de3f5c2fc25fc97452ee56e277e65f1782925
zhanghongyuan111 <945898413@qq.com>
2023-06-21 13:54:33 +0800

    IvorySQL:fix- The source table duplicates data, and the ctid is invalid during merge, resulting in an assertion error

--- contrib/ivorysql_ora/sql/ora_merge.sql
+++ contrib/ivorysql_ora/sql/ora_merge.sql
@@ -402,3 +402,28 @@ ROLLBACK;
 
 drop table members;
 drop table member_staging;
+
+CREATE TABLE target (tid integer, balance integer);
+CREATE TABLE source (sid integer, delta integer);
+
+INSERT INTO target VALUES (1, 10);
+INSERT INTO target VALUES (2, 20);
+INSERT INTO target VALUES (3, 30);
+
+INSERT INTO source VALUES (2, 5);
+INSERT INTO source VALUES (3, 20);
+INSERT INTO source VALUES (2, 5);
+
+SELECT * from target;
+SELECT * from source;
+
+-- Merge statement
+MERGE INTO target t
+USING source s
+ON (t.tid = s.sid)
+WHEN MATCHED THEN
+	UPDATE SET balance = 0
+	DELETE WHERE balance = 0;
+
+drop table target;
+drop table source;
