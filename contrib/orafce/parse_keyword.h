f090e079558168c76a7c0bc7453ca59a05d25cb8
ty <332696245@qq.com>
2023-07-31 16:28:38 +0800

    IvorySQL:Delete orafce

--- contrib/orafce/parse_keyword.h
+++
@@ -1,2 +0,0 @@
-
-extern const char *orafce_scan_keyword(const char *text, int *keycode);
--- contrib/orafce/sql/init.sql
+++
@@ -1,5 +0,0 @@
-\set ECHO none
-set client_min_messages TO error;
-CREATE EXTENSION IF NOT EXISTS orafce;
-GRANT ALL ON SCHEMA public TO public;
-set client_min_messages TO default;
