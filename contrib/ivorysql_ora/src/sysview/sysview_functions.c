f72c404856f5fa1e8401deea0cf6c05805118bc2
ty <332696245@qq.com>
2023-07-31 09:16:32 +0800

    IvorySQL:SUPPORT SRS-SQL-SYSVIEW(_PROCEDURES,_SOURCE,_ARGUMENTS)

---
+++ contrib/ivorysql_ora/src/sysview/sysview_functions.c
@@ -0,0 +1,40 @@
+/*------------------------------------------------------------------
+ * 
+ * File: sysview_functions.c
+ *
+ * Abstract: 
+ * 		Function which converts all-uppercase text to all-lowercase text 
+ * 		and vice versa.
+ *
+ * Copyright:
+ * Copyright (c) 2022, HighGo Software Co.,Ltd. All rights reserved. 
+ *
+ * Identification:
+ *		contrib/ivorysql_ora/src/sysview/sysview_functions.c
+ *
+ *------------------------------------------------------------------
+ */
+
+#include "postgres.h"
+
+#include "fmgr.h"
+#include "parser/scansup.h"
+#include "utils/builtins.h"
+
+PG_FUNCTION_INFO_V1(ora_case_trans);
+Datum
+ora_case_trans(PG_FUNCTION_ARGS)
+{
+	char	*string;
+	char	*retval;
+
+	if (PG_ARGISNULL(0))
+		PG_RETURN_NULL();
+
+	string = (char *) TextDatumGetCString(PG_GETARG_DATUM(0));
+
+	retval = identifier_case_transform(string, strlen(string));
+
+	PG_RETURN_TEXT_P(cstring_to_text(retval));
+}
+
