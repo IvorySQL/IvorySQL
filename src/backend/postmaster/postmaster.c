d6d184e9b483ad0f9cbeb11048ad19370d734ec5
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2023-08-24 17:03:05 +0300

    Use FD_CLOEXEC on ListenSockets
    It's good hygiene if e.g. an extension launches a subprogram when
    being loaded. We went through some effort to close them in the child
    process in EXEC_BACKEND mode, but it's better to not hand them down to
    the child process in the first place. We still need to close them
    after fork when !EXEC_BACKEND, but it's a little simpler.
    
    In the passing, LOG a message if closing the client connection or
    listen socket fails. Shouldn't happen, but if it does, would be nice
    to know.
    
    Reviewed-by: Tristan Partin, Andres Freund, Thomas Munro
    Discussion: https://www.postgresql.org/message-id/7a59b073-5b5b-151e-7ed3-8b01ff7ce9ef@iki.fi

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -505,7 +505,6 @@ typedef struct
 	Port		port;
 	InheritableSocket portsocket;
 	char		DataDir[MAXPGPATH];
-	pgsocket	ListenSocket[MAXLISTEN];
 	int32		MyCancelKey;
 	int			MyPMChildSlot;
 #ifndef WIN32
@@ -2752,8 +2751,6 @@ ConnFree(Port *port)
 void
 ClosePostmasterPorts(bool am_syslogger)
 {
-	int			i;
-
 	/* Release resources held by the postmaster's WaitEventSet. */
 	if (pm_wait_set)
 	{
@@ -2780,8 +2777,12 @@ ClosePostmasterPorts(bool am_syslogger)
 	/*
 	 * Close the postmaster's listen sockets.  These aren't tracked by fd.c,
 	 * so we don't call ReleaseExternalFD() here.
+	 *
+	 * The listen sockets are marked as FD_CLOEXEC, so this isn't needed in
+	 * EXEC_BACKEND mode.
 	 */
-	for (i = 0; i < MAXLISTEN; i++)
+#ifndef EXEC_BACKEND
+	for (int i = 0; i < MAXLISTEN; i++)
 	{
 		if (ListenSocket[i] != PGINVALID_SOCKET)
 		{
@@ -2789,6 +2790,7 @@ ClosePostmasterPorts(bool am_syslogger)
 			ListenSocket[i] = PGINVALID_SOCKET;
 		}
 	}
+#endif
 
 	/*
 	 * If using syslogger, close the read side of the pipe.  We don't bother
@@ -6294,8 +6296,6 @@ save_backend_variables(BackendParameters *param, Port *port,
 
 	strlcpy(param->DataDir, DataDir, MAXPGPATH);
 
-	memcpy(&param->ListenSocket, &ListenSocket, sizeof(ListenSocket));
-
 	param->MyCancelKey = MyCancelKey;
 	param->MyPMChildSlot = MyPMChildSlot;
 
@@ -6526,8 +6526,6 @@ restore_backend_variables(BackendParameters *param, Port *port)
 	read_inheritable_socket(&port->sock, &param->portsocket);
 
 	SetDataDir(param->DataDir);
-
-	memcpy(&ListenSocket, &param->ListenSocket, sizeof(ListenSocket));
 
 	MyCancelKey = param->MyCancelKey;
 	MyPMChildSlot = param->MyPMChildSlot;
