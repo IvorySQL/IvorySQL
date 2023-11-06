063d4d0707e5164093d24988d893f348120961ca
Shawn Yan <shawn2016@aliyun.com>
2023-11-07 00:01:46 +0800

    Update xlog.c
    fix typo

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -4990,7 +4990,7 @@ CleanupAfterArchiveRecovery(TimeLineID EndOfLogTLI, XLogRecPtr EndOfLog,
 	}
 }
 
-/* IvorSQL: BEGIN - case sensitive indentify
+/* IvorySQL: BEGIN - case sensitive indentify
  * Read database compatibility mode from pg_control file
  *
  */
