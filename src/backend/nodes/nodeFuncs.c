56ad2ccf9416b6c28472ba71e64d613cc7a20e21
Tom Lane <tgl@sss.pgh.pa.us>
2024-01-10 13:36:33 -0500

    Handle WindowClause.runCondition in tree walker/mutator functions.
    Commit 9d9c02ccd, which added the notion of a "run condition" for
    window functions, neglected to teach nodeFuncs.c to process the new
    field.  Remarkably, that doesn't seem to have had any ill effects
    before we invented Var.varnullingrels, but now it can cause visible
    failures in join-removal scenarios.
    
    I have no faith that there's not reachable problems in v15 too,
    so back-patch the code change to v15 where 9d9c02ccd came in.
    The test case seems irrelevant to v15, though.
    
    Per bug #18277 from Zuming Jiang.  Diagnosis and patch by
    Richard Guo.
    
    Discussion: https://postgr.es/m/18277-089ead83b329a2fd@postgresql.org

--- src/backend/nodes/nodeFuncs.c
+++ src/backend/nodes/nodeFuncs.c
@@ -2292,6 +2292,8 @@ expression_tree_walker_impl(Node *node,
 					return true;
 				if (WALK(wc->endOffset))
 					return true;
+				if (WALK(wc->runCondition))
+					return true;
 			}
 			break;
 		case T_CTECycleClause:
@@ -2635,6 +2637,8 @@ query_tree_walker_impl(Query *query,
 			if (WALK(wc->startOffset))
 				return true;
 			if (WALK(wc->endOffset))
+				return true;
+			if (WALK(wc->runCondition))
 				return true;
 		}
 	}
@@ -3322,6 +3326,7 @@ expression_tree_mutator_impl(Node *node,
 				MUTATE(newnode->orderClause, wc->orderClause, List *);
 				MUTATE(newnode->startOffset, wc->startOffset, Node *);
 				MUTATE(newnode->endOffset, wc->endOffset, Node *);
+				MUTATE(newnode->runCondition, wc->runCondition, List *);
 				return (Node *) newnode;
 			}
 			break;
@@ -3651,6 +3656,7 @@ query_tree_mutator_impl(Query *query,
 			FLATCOPY(newnode, wc, WindowClause);
 			MUTATE(newnode->startOffset, wc->startOffset, Node *);
 			MUTATE(newnode->endOffset, wc->endOffset, Node *);
+			MUTATE(newnode->runCondition, wc->runCondition, List *);
 
 			resultlist = lappend(resultlist, (Node *) newnode);
 		}
