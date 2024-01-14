d1e20db22e4d45c3bcf0b2a41d4ade1d7ef72f7c
Peter Eisentraut <peter@eisentraut.org>
2024-01-14 07:29:45 +0100

    Remove useless Assert
    It's already included in the subsequent intVal() call.  Fixup for
    4f622503d6.

--- src/backend/commands/tablecmds.c
+++ src/backend/commands/tablecmds.c
@@ -8570,7 +8570,6 @@ ATExecSetStatistics(Relation rel, const char *colName, int16 colNum, Node *newVa
 
 	if (newValue)
 	{
-		Assert(IsA(newValue, Integer));
 		newtarget = intVal(newValue);
 	}
 	else
