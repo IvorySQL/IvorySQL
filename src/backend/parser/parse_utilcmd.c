3744cc4c7559cc4395334442ddb1c73c99052ae4
Peter Eisentraut <peter@eisentraut.org>
2024-02-16 11:51:35 +0100

    Improve compression and storage support with inheritance
    A child table can specify a compression or storage method different
    from its parents.  This was previously an error.  (But this was
    inconsistently enforced because for example the settings could be
    changed later using ALTER TABLE.)  This now also allows an explicit
    override if multiple parents have different compression or storage
    settings, which was previously an error that could not be overridden.
    
    The compression and storage properties remains unchanged in a child
    inheriting from parent(s) after its creation, i.e., when using ALTER
    TABLE ...  INHERIT.  (This is not changed.)
    
    Before this change, the error detail would mention the first pair of
    conflicting parent compression or storage methods.  But with this
    change it waits till the child specification is considered by which
    time we may have encountered many such conflicting pairs.  Hence the
    error detail after this change does not include the conflicting
    compression/storage methods.  Those can be obtained from parent
    definitions if necessary.  The code to maintain list of all
    conflicting methods or even the first conflicting pair does not seem
    worth the convenience it offers.  This change is inline with what we
    do with conflicting default values.
    
    Before this commit, the specified storage method could be stored in
    ColumnDef::storage (CREATE TABLE ... LIKE) or ColumnDef::storage_name
    (CREATE TABLE ...).  This caused the MergeChildAttribute() and
    MergeInheritedAttribute() to ignore a storage method specified in the
    child definition since it looked only at ColumnDef::storage.  This
    commit removes ColumnDef::storage and instead uses
    ColumnDef::storage_name to save any storage method specification. This
    is similar to how compression method specification is handled.
    
    Author: Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/24656cec-d6ef-4d15-8b5b-e8dfc9c833a7@eisentraut.org

--- src/backend/parser/parse_utilcmd.c
+++ src/backend/parser/parse_utilcmd.c
@@ -1170,9 +1170,9 @@ transformTableLikeClause(CreateStmtContext *cxt, TableLikeClause *table_like_cla
 
 		/* Likewise, copy storage if requested */
 		if (table_like_clause->options & CREATE_TABLE_LIKE_STORAGE)
-			def->storage = attribute->attstorage;
+			def->storage_name = GetAttributeStorageName(attribute->attstorage);
 		else
-			def->storage = 0;
+			def->storage_name = NULL;
 
 		/* Likewise, copy invisible if requested */
 		if (table_like_clause->options & CREATE_TABLE_LIKE_INVISIBLE)
@@ -1183,8 +1183,7 @@ transformTableLikeClause(CreateStmtContext *cxt, TableLikeClause *table_like_cla
 		/* Likewise, copy compression if requested */
 		if ((table_like_clause->options & CREATE_TABLE_LIKE_COMPRESSION) != 0
 			&& CompressionMethodIsValid(attribute->attcompression))
-			def->compression =
-				pstrdup(GetCompressionMethodName(attribute->attcompression));
+			def->compression = GetCompressionMethodName(attribute->attcompression);
 		else
 			def->compression = NULL;
 
