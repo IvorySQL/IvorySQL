1dfbaec8714470e32d3e2cba51b6894d454a3f68
Robert Haas <rhaas@postgresql.org>
2024-03-29 08:44:45 -0400

    Add allow_alter_system GUC.
    This is marked PGC_SIGHUP, so it can only be set in a configuration
    file, not anywhere else; and it is also marked GUC_DISALLOW_IN_AUTO_FILE,
    so it can't be set using ALTER SYSTEM. When set to false, the
    ALTER SYSTEM command is disallowed.
    
    There was considerable concern that this would be misinterpreted as
    a security feature, which it is not, because a determined superuser
    has various ways of bypassing it. Hence, a lot of work has gone into
    wordsmithing the documentation, in the hopes of avoiding any such
    confusion.
    
    Jelte Fennemia-Nio and Gabriele Bartolini, with wording suggestions
    for the documentation from many others.
    
    Discussion: http://postgr.es/m/CA%2BVUV5rEKt2%2BCdC_KUaPoihMu%2Bi5ChT4WVNTr4CD5-xXZUfuQw%40mail.gmail.com

--- src/include/utils/guc.h
+++ src/include/utils/guc.h
@@ -255,6 +255,7 @@ extern PGDLLIMPORT bool check_function_bodies;
 extern PGDLLIMPORT bool	enable_emptystring_to_NULL;
 extern PGDLLIMPORT bool current_role_is_superuser;
 
+extern PGDLLIMPORT bool AllowAlterSystem;
 extern PGDLLIMPORT bool log_duration;
 extern PGDLLIMPORT int log_parameter_max_length;
 extern PGDLLIMPORT int log_parameter_max_length_on_error;
