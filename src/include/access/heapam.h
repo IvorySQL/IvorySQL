d8c200970c4d8171d8944d9cec990a0aeb60c713
Thomas Munro <tmunro@postgresql.org>
2024-04-08 13:16:20 +1200

    Use streaming I/O in ANALYZE.
    The ANALYZE command prefetches and reads sample blocks chosen by a
    BlockSampler algorithm. Instead of calling [Prefetch|Read]Buffer() for
    each block, ANALYZE now uses the streaming API introduced in b5a9b18cd0.
    
    Author: Nazir Bilal Yavuz <byavuz81@gmail.com>
    Reviewed-by: Melanie Plageman <melanieplageman@gmail.com>
    Reviewed-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Jakub Wartak <jakub.wartak@enterprisedb.com>
    Reviewed-by: Heikki Linnakangas <hlinnaka@iki.fi>
    Reviewed-by: Thomas Munro <thomas.munro@gmail.com>
    Discussion: https://postgr.es/m/flat/CAN55FZ0UhXqk9v3y-zW_fp4-WCp43V8y0A72xPmLkOM%2B6M%2BmJg%40mail.gmail.com

--- src/include/access/heapam.h
+++ src/include/access/heapam.h
@@ -562,9 +562,8 @@ extern void compute_new_xmax_infomask(TransactionId xmax, uint16 old_infomask,
 extern XLogRecPtr log_heap_new_cid(Relation relation, HeapTuple tup);
 extern uint8 compute_infobits(uint16 infomask, uint16 infomask2);
 /* in heap/heapam_handler.c*/
-extern void heapam_scan_analyze_next_block(TableScanDesc scan,
-										   BlockNumber blockno,
-										   BufferAccessStrategy bstrategy);
+extern bool heapam_scan_analyze_next_block(TableScanDesc scan,
+										   ReadStream *stream);
 extern bool heapam_scan_analyze_next_tuple(TableScanDesc scan,
 										   TransactionId OldestXmin,
 										   double *liverows, double *deadrows,
