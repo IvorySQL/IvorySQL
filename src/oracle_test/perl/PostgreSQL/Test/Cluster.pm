03ce8e08e134becbf44fff9c14b292dfccd8de7d
JiaoShuntian <jiaoshuntian@highgo.com>
2024-05-30 14:44:45 +0800

    update IvorySQL regression test with pg commit 995d400

--- src/oracle_test/perl/PostgreSQL/Test/Cluster.pm
+++ src/oracle_test/perl/PostgreSQL/Test/Cluster.pm
@@ -114,8 +114,9 @@ use Socket;
 use Test::More;
 use PostgreSQL::Test::Utils ();
 use PostgreSQL::Test::BackgroundPsql ();
-use Time::HiRes qw(usleep);
-use Scalar::Util qw(blessed);
+use Text::ParseWords                 qw(shellwords);
+use Time::HiRes                      qw(usleep);
+use Scalar::Util                     qw(blessed);
 
 our ($use_tcp, $test_localhost, $test_pghost, $last_host_assigned,
 	$last_port_assigned, @all_nodes, $died, $portdir);
@@ -518,6 +519,12 @@ sub init
 
 	$params{allows_streaming} = 0 unless defined $params{allows_streaming};
 	$params{has_archiving} = 0 unless defined $params{has_archiving};
+
+	my $initdb_extra_opts_env = $ENV{PG_TEST_INITDB_EXTRA_OPTS};
+	if (defined $initdb_extra_opts_env)
+	{
+		push @{ $params{extra} }, shellwords($initdb_extra_opts_env);
+	}
 
 	mkdir $self->backup_dir;
 	mkdir $self->archive_dir;
