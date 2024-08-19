c3e576274850a0763acc470130734dd40df7dc5b
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-08-19 09:48:25 +0300

    Fix garbled process name on backend crash
    The log message on backend crash used wrong variable, which could be
    uninitialized. Introduced in commit 28a520c0b7.
    
    Reported-by: Alexander Lakhin
    Discussion: https://www.postgresql.org/message-id/451b0797-83b8-cdbc-727f-8d7a7b0e3bca@gmail.com

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -2723,7 +2723,7 @@ CleanupBackend(Backend *bp,
 
 	if (crashed)
 	{
-		HandleChildCrash(bp->pid, exitstatus, namebuf);
+		HandleChildCrash(bp->pid, exitstatus, procname);
 		pfree(bp);
 		return;
 	}
