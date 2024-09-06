33edfe482774c1ad8546ed527aaed240a8c5a4c8
Amit Langote <amitlan@postgresql.org>
2024-09-06 13:25:02 +0900

    SQL/JSON: Fix JSON_TABLE() column deparsing
    The deparsing code in get_json_expr_options() unnecessarily emitted
    the default column-specific ON ERROR / EMPTY behavior when the
    top-level ON ERROR behavior in JSON_TABLE was set to ERROR. Fix that
    by not overriding the column-specific default, determined based on
    the column's JsonExprOp in get_json_table_columns(), with
    JSON_BEHAVIOR_ERROR when that is the top-level ON ERROR behavior.
    
    Note that this only removes redundancy; the current deparsing output
    is not incorrect, just redundant.
    
    Reviewed-by: Jian He <jian.universality@gmail.com>
    Discussion: https://postgr.es/m/CACJufxEo4sUjKCYtda0_qt9tazqqKPmF1cqhW9KBOUeJFqQd2g@mail.gmail.com
    Backpatch-through: 17

--- src/backend/utils/adt/ruleutils.c
+++ src/backend/utils/adt/ruleutils.c
@@ -12041,7 +12041,6 @@ get_json_table_columns(TableFunc *tf, JsonTablePathScan *scan,
 					   bool showimplicit)
 {
 	StringInfo	buf = context->buf;
-	JsonExpr   *jexpr = castNode(JsonExpr, tf->docexpr);
 	ListCell   *lc_colname;
 	ListCell   *lc_coltype;
 	ListCell   *lc_coltypmod;
@@ -12094,6 +12093,10 @@ get_json_table_columns(TableFunc *tf, JsonTablePathScan *scan,
 		if (ordinality)
 			continue;
 
+		/*
+		 * Set default_behavior to guide get_json_expr_options() on whether to
+		 * to emit the ON ERROR / EMPTY clauses.
+		 */
 		if (colexpr->op == JSON_EXISTS_OP)
 		{
 			appendStringInfoString(buf, " EXISTS");
@@ -12116,9 +12119,6 @@ get_json_table_columns(TableFunc *tf, JsonTablePathScan *scan,
 
 			default_behavior = JSON_BEHAVIOR_NULL;
 		}
-
-		if (jexpr->on_error->btype == JSON_BEHAVIOR_ERROR)
-			default_behavior = JSON_BEHAVIOR_ERROR;
 
 		appendStringInfoString(buf, " PATH ");
 
