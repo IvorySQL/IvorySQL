sanchitachaurasia/DBIS-Project

Remove last NULL element in config_group_names[]
This has not been needed since 9d77708d83ee where there was a loop to
print all the possible GUC groups, relying on the last element to be
NULL.

Author: Japin Li
Reviewed-By: Jelte Fennema-Nio
Discussion: https://postgr.es/m/CAGECzQT3caUbcCcszNewCCmMbCuyP7XNAm60J3ybd6PN5kH2Dw@mail.gmail.com

src/backend/utils/misc/guc_tables.c
@@ -715,11 +715,9 @@ const char *const config_group_names[] =
 	[PRESET_OPTIONS] = gettext_noop("Preset Options"),
 	[CUSTOM_OPTIONS] = gettext_noop("Customized Options"),
 	[DEVELOPER_OPTIONS] = gettext_noop("Developer Options"),
-	/* help_config wants this array to be null-terminated */
-	NULL
 };
 
-StaticAssertDecl(lengthof(config_group_names) == (DEVELOPER_OPTIONS + 2),
+StaticAssertDecl(lengthof(config_group_names) == (DEVELOPER_OPTIONS + 1),
 				 "array length mismatch");
 
 /*

IvorySQL/IvorySQL

Conflicts:
	src/backend/utils/misc/guc_tables.c

src/backend/utils/misc/guc_tables.c
@@ -742,11 +742,9 @@ const char *const config_group_names[] =
 	[CUSTOM_OPTIONS] = gettext_noop("Customized Options"),
 	[DEVELOPER_OPTIONS] = gettext_noop("Developer Options"),
 	[COMPAT_ORACLE_OPTIONS] = gettext_noop("Compat Oracle Options"),
-	/* help_config wants this array to be null-terminated */
-	NULL
 };
 
-StaticAssertDecl(lengthof(config_group_names) == (COMPAT_ORACLE_OPTIONS + 2),
+StaticAssertDecl(lengthof(config_group_names) == (COMPAT_ORACLE_OPTIONS + 1),
 				 "array length mismatch");
 
 /*