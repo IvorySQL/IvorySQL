9d0097ac73e772b1bbd99a0435644909ba1be1bf
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2024-11-01 13:47:24 +0200

    Rename two functions that wake up other processes
    Instead of talking about setting latches, which is a pretty low-level
    mechanism, emphasize that they wake up other processes.
    
    This is in preparation for replacing Latches with a new abstraction.
    That's still work in progress, but this seems a little tidier anyway,
    so let's get this refactoring out of the way already.
    
    Discussion: https://www.postgresql.org/message-id/391abe21-413e-4d91-a650-b663af49500c%40iki.fi

--- src/backend/access/transam/xlog.c
+++ src/backend/access/transam/xlog.c
@@ -6311,7 +6311,7 @@ StartupXLOG(void)
 	 * Wake up all waiters for replay LSN.  They need to report an error that
 	 * recovery was ended before reaching the target LSN.
 	 */
-	WaitLSNSetLatches(InvalidXLogRecPtr);
+	WaitLSNWakeup(InvalidXLogRecPtr);
 
 	/*
 	 * Shutdown the recovery environment.  This must occur after
@@ -7436,7 +7436,7 @@ CreateCheckPoint(int flags)
 	 * until after the above call that flushes the XLOG_CHECKPOINT_ONLINE
 	 * record.
 	 */
-	SetWalSummarizerLatch();
+	WakeupWalSummarizer();
 
 	/*
 	 * Let smgr do post-checkpoint cleanup (eg, deleting old files).
