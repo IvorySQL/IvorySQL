1f8b8c78ab47e1d7aebb368c6877b169934e8aac
Daniel Gustafsson <dgustafsson@postgresql.org>
2024-11-06 15:40:52 +0100

    Find invalid databases during upgrade check stage
    Before continuing with the check start by checking that all databases
    allow connections to avoid a hard fail without proper error reporting.
    
    Inspired by a larger patch by Thomas Krennwallner.
    
    Discussion: https://postgr.es/m/f9315bf0-e03e-4490-9f0d-5b6f7a6d9908@postsubmeta.net

--- src/bin/pg_upgrade/t/002_pg_upgrade.pl
+++ src/bin/pg_upgrade/t/002_pg_upgrade.pl
@@ -425,7 +425,7 @@ SKIP:
 			$mode, '--check',
 		],
 		1,
-		[qr/invalid/],    # pg_upgrade prints errors on stdout :(
+		[qr/datconnlimit/],
 		[qr/^$/],
 		'invalid database causes failure');
 	rmtree($newnode->data_dir . "/pg_upgrade_output.d");
