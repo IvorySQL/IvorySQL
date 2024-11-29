739f3799703c9a4de66fc7b7ff8d3df704b9cbdd
Steven Niu <6902481@qq.com>
2024-11-29 05:08:03 +0000

    IvorySQL: update oracle code for pg commit 7d2c7f0

--- src/oracle_test/regress/sql/window.sql
+++ src/oracle_test/regress/sql/window.sql
@@ -1187,6 +1187,14 @@ SELECT 1 FROM
    WHERE e1.empno = e2.empno) s
 WHERE s.c = 1;
 
+-- Ensure the run condition optimization is used in cases where the WindowFunc
+-- has a Var from another query level
+EXPLAIN (COSTS OFF)
+SELECT 1 FROM
+  (SELECT ntile(s1.x) OVER () AS c
+   FROM (SELECT (SELECT 1) AS x) AS s1) s
+WHERE s.c = 1;
+
 -- Test Sort node collapsing
 EXPLAIN (COSTS OFF)
 SELECT * FROM
