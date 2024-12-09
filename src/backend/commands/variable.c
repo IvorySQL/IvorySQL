d4e3f6e28d179836e16b322bb0a9a10fa4e5546c
Daniel Gustafsson <dgustafsson@postgresql.org>
2024-12-09 20:58:23 +0100

    Fix small memory leaks in GUC checks
    Follow-up commit to a9d58bfe8a3a.  Backpatch down to v16 where
    this was added in order to keep the code consistent for future
    backpatches.
    
    Author: Tofig Aliev <t.aliev@postgrespro.ru>
    Reviewed-by: Daniel Gustafsson <daniel@yesql.se>
    Reviewed-by: Masahiko Sawada <sawada.mshk@gmail.com>
    Discussion: https://postgr.es/m/bba4313fdde9db46db279f96f3b748b1@postgrespro.ru
    Backpatch-through: 16

--- src/backend/commands/variable.c
+++ src/backend/commands/variable.c
@@ -1093,6 +1093,8 @@ check_application_name(char **newval, void **extra, GucSource source)
 		return false;
 	}
 
+	guc_free(*newval);
+
 	pfree(clean);
 	*newval = ret;
 	return true;
@@ -1128,6 +1130,8 @@ check_cluster_name(char **newval, void **extra, GucSource source)
 		pfree(clean);
 		return false;
 	}
+
+	guc_free(*newval);
 
 	pfree(clean);
 	*newval = ret;
