e17f4df8f507def1c722327f99cc876104ad0bb0
Tom Lane <tgl@sss.pgh.pa.us>
2024-12-28 16:21:54 -0500

    Replace PGPROC.isBackgroundWorker with isRegularBackend.
    Commit 34486b609 effectively redefined isBackgroundWorker as meaning
    "not a regular backend", whereas before it had the narrower
    meaning of AmBackgroundWorkerProcess().  For clarity, rename the
    field to isRegularBackend and invert its sense.
    
    Discussion: https://postgr.es/m/1808397.1735156190@sss.pgh.pa.us

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -433,7 +433,7 @@ InitProcess(void)
 	MyProc->databaseId = InvalidOid;
 	MyProc->roleId = InvalidOid;
 	MyProc->tempNamespaceId = InvalidOid;
-	MyProc->isBackgroundWorker = !AmRegularBackendProcess();
+	MyProc->isRegularBackend = AmRegularBackendProcess();
 	MyProc->delayChkptFlags = 0;
 	MyProc->statusFlags = 0;
 	/* NB -- autovac launcher intentionally does not set IS_AUTOVACUUM */
@@ -632,7 +632,7 @@ InitAuxiliaryProcess(void)
 	MyProc->databaseId = InvalidOid;
 	MyProc->roleId = InvalidOid;
 	MyProc->tempNamespaceId = InvalidOid;
-	MyProc->isBackgroundWorker = true;
+	MyProc->isRegularBackend = false;
 	MyProc->delayChkptFlags = 0;
 	MyProc->statusFlags = 0;
 	MyProc->lwWaiting = LW_WS_NOT_WAITING;
