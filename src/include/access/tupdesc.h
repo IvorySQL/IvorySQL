bafe8b801b8004faa2e3084c825eec5df8bfa242
David Rowley <drowley@postgresql.org>
2025-01-11 13:45:54 +1300

    Make verify_compact_attribute available in non-assert builds
    6f3820f37 adjusted the assert-enabled validation of the CompactAttribute
    to call a new external function to perform the validation.  That commit
    made it so the function was only available when building with
    USE_ASSERT_CHECKING, and because TupleDescCompactAttr() is a static
    inline function, the call to verify_compact_attribute() was compiled
    into any extension which uses TupleDescCompactAttr().  This caused issues
    for such extensions when loading the assert-enabled extension into
    PostgreSQL versions without asserts enabled due to that function being
    unavailable in core.
    
    To fix this, make verify_compact_attribute() available unconditionally,
    but make it do nothing unless building with USE_ASSERT_CHECKING.
    
    Author: Andrew Kane <andrew@ankane.org>
    Reviewed-by: David Rowley <dgrowleyml@gmail.com>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/CAOdR5yHfMEMW00XGo=v1zCVUS6Huq2UehXdvKnwtXPTcZwXhmg@mail.gmail.com

--- src/include/access/tupdesc.h
+++ src/include/access/tupdesc.h
@@ -159,9 +159,7 @@ TupleDescAttr(TupleDesc tupdesc, int i)
 
 #undef TupleDescAttrAddress
 
-#ifdef USE_ASSERT_CHECKING
 extern void verify_compact_attribute(TupleDesc, int attnum);
-#endif
 
 /*
  * Accessor for the i'th CompactAttribute element of tupdesc.
