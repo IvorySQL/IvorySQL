df23b7fa5a27004797d15941cef6404aefb8617e
Peter Eisentraut <peter@eisentraut.org>
2025-01-15 08:31:46 +0100

    Rename RowCompareType to CompareType
    RowCompareType served as a way to describe the fundamental meaning of
    an operator, notionally independent of an operator class (although so
    far this was only really supported for btrees).  Its original purpose
    was for use inside RowCompareExpr, and it has also found some small
    use outside, such as for get_op_btree_interpretation().
    
    We want to expand this now, as a more general way to describe operator
    semantics for other index access methods, including gist (to improve
    GistTranslateStratnum()) and others not written yet.  To avoid future
    confusion, we rename the type to CompareType and the symbols from
    ROWCOMPARE_XXX to COMPARE_XXX to reflect their more general purpose.
    
    Reviewed-by: Mark Dilger <mark.dilger@enterprisedb.com>
    Discussion: https://www.postgresql.org/message-id/flat/E72EAA49-354D-4C2E-8EB9-255197F55330@enterprisedb.com

--- src/include/executor/execExpr.h
+++ src/include/executor/execExpr.h
@@ -505,7 +505,7 @@ typedef struct ExprEvalStep
 		/* for EEOP_ROWCOMPARE_FINAL */
 		struct
 		{
-			RowCompareType rctype;
+			CompareType cmptype;
 		}			rowcompare_final;
 
 		/* for EEOP_MINMAX */
