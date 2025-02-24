799dff455311d446fcfbb457af2a3512b53edc36
Michael Paquier <michael@paquier.xyz>
2025-02-24 09:51:56 +0900

    Remove read/sync fields from pg_stat_wal and GUC track_wal_io_timing
    The four following attributes are removed from pg_stat_wal:
    * wal_write
    * wal_sync
    * wal_write_time
    * wal_sync_time
    
    a051e71e28a1 has added an equivalent of this information in pg_stat_io
    with more granularity as this now spreads across the backend types, IO
    context and IO objects.  So, keeping the same information in pg_stat_wal
    has little benefits.
    
    Another benefit of this commit is the removal of PendingWalStats,
    simplifying an upcoming patch to add per-backend WAL statistics, which
    already support IO statistics and which have access to the write/sync
    stats data of WAL.
    
    The GUC track_wal_io_timing, that was used to enable or disable the
    aggregation of the write and sync timings for WAL, is also removed.
    pgstat_prepare_io_time() is simplified.
    
    Bump catalog version.
    Bump PGSTAT_FILE_FORMAT_ID, due to the update of PgStat_WalStats.
    
    Author: Bertrand Drouvot <bertranddrouvot.pg@gmail.com>
    Discussion: https://postgr.es/m/Z7RkQ0EfYaqqjgz/@ip-10-97-1-34.eu-west-3.compute.internal

--- src/backend/storage/buffer/bufmgr.c
+++ src/backend/storage/buffer/bufmgr.c
@@ -1510,7 +1510,7 @@ WaitReadBuffers(ReadBuffersOperation *operation)
 			io_pages[io_buffers_len++] = BufferGetBlock(buffers[i]);
 		}
 
-		io_start = pgstat_prepare_io_time(track_io_timing);
+		io_start = pgstat_prepare_io_time();
 		smgrreadv(operation->smgr, forknum, io_first_block, io_pages, io_buffers_len);
 		pgstat_count_io_op_time(io_object, io_context, IOOP_READ, io_start,
 								1, io_buffers_len * BLCKSZ);
@@ -2402,7 +2402,7 @@ ExtendBufferedRelShared(BufferManagerRelation bmr,
 		}
 	}
 
-	io_start = pgstat_prepare_io_time(track_io_timing);
+	io_start = pgstat_prepare_io_time();
 
 	/*
 	 * Note: if smgrzeroextend fails, we will end up with buffers that are
@@ -3859,7 +3859,7 @@ FlushBuffer(BufferDesc *buf, SMgrRelation reln, IOObject io_object,
 	 */
 	bufToWrite = PageSetChecksumCopy((Page) bufBlock, buf->tag.blockNum);
 
-	io_start = pgstat_prepare_io_time(track_io_timing);
+	io_start = pgstat_prepare_io_time();
 
 	/*
 	 * bufToWrite is either the shared buffer or a copy, as appropriate.
@@ -4461,7 +4461,7 @@ FlushRelationBuffers(Relation rel)
 
 				PageSetChecksumInplace(localpage, bufHdr->tag.blockNum);
 
-				io_start = pgstat_prepare_io_time(track_io_timing);
+				io_start = pgstat_prepare_io_time();
 
 				smgrwrite(srel,
 						  BufTagGetForkNum(&bufHdr->tag),
@@ -5918,7 +5918,7 @@ IssuePendingWritebacks(WritebackContext *wb_context, IOContext io_context)
 	sort_pending_writebacks(wb_context->pending_writebacks,
 							wb_context->nr_pending);
 
-	io_start = pgstat_prepare_io_time(track_io_timing);
+	io_start = pgstat_prepare_io_time();
 
 	/*
 	 * Coalesce neighbouring writes, but nothing else. For that we iterate
