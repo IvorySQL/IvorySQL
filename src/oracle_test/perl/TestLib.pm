8ddc2206c9798c7f2e4b948ff945b7a6d3c5971a
JiaoShuntian <jiaoshuntian@highgo.com>
2025-03-04 16:30:03 +0800

    IvorySQL:update IvorySQL regression test with pg commit 3ce3b53

--- src/oracle_test/perl/TestLib.pm
+++ src/oracle_test/perl/TestLib.pm
@@ -70,6 +70,7 @@ our @EXPORT = qw(
   chmod_recursive
   check_pg_config
   dir_symlink
+  scan_server_header
   system_or_bail
   system_log
   run_log
@@ -648,7 +649,48 @@ sub chmod_recursive
 
 =pod
 
-=item check_pg_config(regexp)
+
+can_server_header(header_path, regexp)
+
+Returns an array that stores all the matches of the given regular expression
+within the PostgreSQL installation's C<header_path>.  This can be used to
+retrieve specific value patterns from the installation's header files.
+
+=cut
+
+sub scan_server_header
+{
+	my ($header_path, $regexp) = @_;
+
+	my ($stdout, $stderr);
+	my $result = IPC::Run::run [ 'pg_config', '--includedir-server' ], '>',
+	  \$stdout, '2>', \$stderr
+	  or die "could not execute pg_config";
+	chomp($stdout);
+	$stdout =~ s/\r$//;
+
+	open my $header_h, '<', "$stdout/$header_path" or die "$!";
+
+	my @match = undef;
+	while (<$header_h>)
+	{
+		my $line = $_;
+
+		if (@match = $line =~ /^$regexp/)
+		{
+			last;
+		}
+	}
+
+	close $header_h;
+	die "could not find match in header $header_path\n"
+	  unless @match;
+	return @match;
+}
+
+=pod
+
+item check_pg_config(regexp)
 
 Return the number of matches of the given regular expression
 within the installation's C<pg_config.h>.
