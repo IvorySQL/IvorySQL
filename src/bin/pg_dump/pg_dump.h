f36321fba3ba63e77d57ace013b9541be7a220b3
Noah Misch <noah@leadboat.com>
2025-07-31 06:37:56 -0700

    Sort dump objects independent of OIDs, for the 7 holdout object types.
    pg_dump sorts objects by their logical names, e.g. (nspname, relname,
    tgname), before dependency-driven reordering.  That removes one source
    of logically-identical databases differing in their schema-only dumps.
    In other words, it helps with schema diffing.  The logical name sort
    ignored essential sort keys for constraints, operators, PUBLICATION
    ... FOR TABLE, PUBLICATION ... FOR TABLES IN SCHEMA, operator classes,
    and operator families.  pg_dump's sort then depended on object OID,
    yielding spurious schema diffs.  After this change, OIDs affect dump
    order only in the event of catalog corruption.  While pg_dump also
    wrongly ignored pg_collation.collencoding, CREATE COLLATION restrictions
    have been keeping that imperceptible in practical use.
    
    Use techniques like we use for object types already having full sort key
    coverage.  Where the pertinent queries weren't fetching the ignored sort
    keys, this adds columns to those queries and stores those keys in memory
    for the long term.
    
    The ignorance of sort keys became more problematic when commit
    172259afb563d35001410dc6daad78b250924038 added a schema diff test
    sensitive to it.  Buildfarm member hippopotamus witnessed that.
    However, dump order stability isn't a new goal, and this might avoid
    other dump comparison failures.  Hence, back-patch to v13 (all supported
    versions).
    
    Reviewed-by: Robert Haas <robertmhaas@gmail.com>
    Discussion: https://postgr.es/m/20250707192654.9e.nmisch@google.com
    Backpatch-through: 13

--- src/bin/pg_dump/pg_dump.h
+++ src/bin/pg_dump/pg_dump.h
@@ -266,6 +266,8 @@ typedef struct _oprInfo
 	DumpableObject dobj;
 	const char *rolname;
 	char		oprkind;
+	Oid			oprleft;
+	Oid			oprright;
 	Oid			oprcode;
 } OprInfo;
 
@@ -279,12 +281,14 @@ typedef struct _accessMethodInfo
 typedef struct _opclassInfo
 {
 	DumpableObject dobj;
+	Oid			opcmethod;
 	const char *rolname;
 } OpclassInfo;
 
 typedef struct _opfamilyInfo
 {
 	DumpableObject dobj;
+	Oid			opfmethod;
 	const char *rolname;
 } OpfamilyInfo;
 
@@ -292,6 +296,7 @@ typedef struct _collInfo
 {
 	DumpableObject dobj;
 	const char *rolname;
+	int			collencoding;
 } CollInfo;
 
 typedef struct _convInfo
@@ -737,6 +742,7 @@ extern TableInfo *findTableByOid(Oid oid);
 extern TypeInfo *findTypeByOid(Oid oid);
 extern FuncInfo *findFuncByOid(Oid oid);
 extern OprInfo *findOprByOid(Oid oid);
+extern AccessMethodInfo *findAccessMethodByOid(Oid oid);
 extern CollInfo *findCollationByOid(Oid oid);
 extern NamespaceInfo *findNamespaceByOid(Oid oid);
 extern ExtensionInfo *findExtensionByOid(Oid oid);
