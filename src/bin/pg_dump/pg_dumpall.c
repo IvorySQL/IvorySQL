78c8d82b3293afa76437883b3f08e4cc235c9dd7
Noah Misch <noah@leadboat.com>
2025-08-11 06:18:59 -0700

    Convert newlines to spaces in names written in v11+ pg_dump comments.
    Maliciously-crafted object names could achieve SQL injection during
    restore.  CVE-2012-0868 fixed this class of problem at the time, but
    later work reintroduced three cases.  Commit
    bc8cd50fefd369b217f80078585c486505aafb62 (back-patched to v11+ in
    2023-05 releases) introduced the pg_dump case.  Commit
    6cbdbd9e8d8f2986fde44f2431ed8d0c8fce7f5d (v12+) introduced the two
    pg_dumpall cases.  Move sanitize_line(), unchanged, to dumputils.c so
    pg_dumpall has access to it in all supported versions.  Back-patch to
    v13 (all supported versions).
    
    Reviewed-by: Robert Haas <robertmhaas@gmail.com>
    Reviewed-by: Nathan Bossart <nathandbossart@gmail.com>
    Backpatch-through: 13
    Security: CVE-2025-8715

--- src/bin/pg_dump/pg_dumpall.c
+++ src/bin/pg_dump/pg_dumpall.c
@@ -1499,7 +1499,13 @@ dumpUserConfig(PGconn *conn, const char *username)
 	res = executeQuery(conn, buf->data);
 
 	if (PQntuples(res) > 0)
-		fprintf(OPF, "\n--\n-- User Config \"%s\"\n--\n\n", username);
+	{
+		char	   *sanitized;
+
+		sanitized = sanitize_line(username, true);
+		fprintf(OPF, "\n--\n-- User Config \"%s\"\n--\n\n", sanitized);
+		free(sanitized);
+	}
 
 	for (int i = 0; i < PQntuples(res); i++)
 	{
@@ -1601,6 +1607,7 @@ dumpDatabases(PGconn *conn)
 	for (i = 0; i < PQntuples(res); i++)
 	{
 		char	   *dbname = PQgetvalue(res, i, 0);
+		char	   *sanitized;
 		const char *create_opts;
 		int			ret;
 
@@ -1617,7 +1624,9 @@ dumpDatabases(PGconn *conn)
 
 		pg_log_info("dumping database \"%s\"", dbname);
 
-		fprintf(OPF, "--\n-- Database \"%s\" dump\n--\n\n", dbname);
+		sanitized = sanitize_line(dbname, true);
+		fprintf(OPF, "--\n-- Database \"%s\" dump\n--\n\n", sanitized);
+		free(sanitized);
 
 		/*
 		 * We assume that "template1", "postgres", and "ivorysql" already exist in the
