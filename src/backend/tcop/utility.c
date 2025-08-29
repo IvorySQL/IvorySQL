a50570928f4674bee849f4cba172a97eea37d910
Álvaro Herrera <alvherre@kurilemu.de>
2025-08-29 14:43:47 +0200

    CREATE STATISTICS: improve misleading error message
    I think the error message for a different condition was inadvertently
    copied.
    
    This problem seems to have been introduced by commit a4d75c86bf15.
    
    Author: Álvaro Herrera <alvherre@kurilemu.de>
    Reported-by: jian he <jian.universality@gmail.com>
    Reviewed-by: Tom Lane <tgl@sss.pgh.pa.us>
    Backpatch-through: 14
    Discussion: https://postgr.es/m/CACJufxEZ48toGH0Em_6vdsT57Y3L8pLF=DZCQ_gCii6=C3MeXw@mail.gmail.com

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -1893,7 +1893,8 @@ ProcessUtilitySlow(ParseState *pstate,
 					if (!IsA(rel, RangeVar))
 						ereport(ERROR,
 								(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
-								 errmsg("only a single relation is allowed in CREATE STATISTICS")));
+								 errmsg("cannot create statistics on the specified relation"),
+								 errdetail("CREATE STATISTICS only supports tables, foreign tables and materialized views.")));
 
 					/*
 					 * CREATE STATISTICS will influence future execution plans
