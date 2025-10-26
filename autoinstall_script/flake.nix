41fe1c8017adf25295cea2af659c7fda720ec1e0
yangchunwanwusheng <wangshuo66_0630@qq.com>
2025-10-26 18:35:23 +0800

    Revise according to the review comments

--- autoinstall_script/flake.nix
+++ autoinstall_script/flake.nix
@@ -7,38 +7,42 @@
 
   outputs = { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
     in
     {
-      devShells.${system}.default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          # Core compiler and build tools
-          gcc
-          gnumake
-          bison
-          flex
-          pkg-config
-          # Dependencies required for building IvorySQL
-          readline.dev
-          zlib.dev
-          openssl.dev
-          libxml2.dev
-          libxslt
-          icu.dev
-          libuuid
-          gettext
-          tcl
-          # Python development environment
-          python3
-          python3Packages.setuptools
-          # For running the regression test suite
-          perl
-          perlPackages.IPCRun
-          # Additional utilities
-          which
-          curl
-        ];
-      };
+      devShells = forAllSystems (system: 
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              # Core compiler and build tools
+              gcc
+              gnumake
+              bison
+              flex
+              pkg-config
+              # Dependencies required for building IvorySQL
+              readline.dev
+              zlib.dev
+              openssl.dev
+              libxml2.dev
+              libxslt
+              icu.dev
+              libuuid
+              gettext
+              tcl
+              # Python development environment
+              python3
+              python3Packages.setuptools
+              # For running the regression test suite
+              perl
+              perlPackages.IPCRun
+              # Additional utilities
+              which
+              curl
+            ];
+          };
+        });
     };
 }
