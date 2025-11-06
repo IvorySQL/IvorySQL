583b5220b9ee0cf41eb8f851e64b00efe68d857c
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2025-11-06 14:45:00 +0200

    Refactor shared memory allocation for semaphores
    Before commit e25626677f, spinlocks were implemented using semaphores
    on some platforms (--disable-spinlocks). That made it necessary to
    initialize semaphores early, before any spinlocks could be used. Now
    that we don't support --disable-spinlocks anymore, we can allocate the
    shared memory needed for semaphores the same way as other shared
    memory structures. Since the semaphores are used only in the PGPROC
    array, move the semaphore shmem size estimation and initialization
    calls to ProcGlobalShmemSize() and InitProcGlobal().
    
    Author: Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
    Discussion: https://www.postgresql.org/message-id/CAExHW5seSZpPx-znjidVZNzdagGHOk06F+Ds88MpPUbxd1kTaA@mail.gmail.com

--- src/backend/storage/lmgr/proc.c
+++ src/backend/storage/lmgr/proc.c
@@ -146,6 +146,7 @@ ProcGlobalShmemSize(void)
 	size = add_size(size, sizeof(PROC_HDR));
 	size = add_size(size, sizeof(slock_t));
 
+	size = add_size(size, PGSemaphoreShmemSize(ProcGlobalSemas()));
 	size = add_size(size, PGProcShmemSize());
 	size = add_size(size, FastPathLockShmemSize());
 
@@ -287,6 +288,9 @@ InitProcGlobal(void)
 
 	/* For asserts checking we did not overflow. */
 	fpEndPtr = fpPtr + requestSize;
+
+	/* Reserve space for semaphores. */
+	PGReserveSemaphores(ProcGlobalSemas());
 
 	for (i = 0; i < TotalProcs; i++)
 	{
