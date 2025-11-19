57b09242213d807e2aeff8fe05fd2a3bd8f0568a
Peter Eisentraut <peter@eisentraut.org>
2025-11-19 08:02:05 +0100

    Add <stdalign.h> to c.h
    This allows using the C11 constructs alignas and alignof (not done in
    this patch).
    
    Reviewed-by: Chao Li <li.evan.chao@gmail.com>
    Discussion: https://www.postgresql.org/message-id/flat/46f05236-d4d4-4b4e-84d4-faa500f14691%40eisentraut.org

--- src/include/c.h
+++ src/include/c.h
@@ -61,6 +61,7 @@
 
 /* System header files that should be available everywhere in Postgres */
 #include <inttypes.h>
+#include <stdalign.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
