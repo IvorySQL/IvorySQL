0ad7c66ae54b62cde93877a667e45e36bf259013
Yasir.Hussain.Shah <yasir.hussain.shah@data-bene.io>
2026-01-27 20:52:36 +0500

    add regression: tests

--- contrib/ivorysql_ora/sql/utl_file.sql
+++ contrib/ivorysql_ora/sql/utl_file.sql
@@ -1,121 +1,165 @@
+--
+-- tests for utl_file package
+--
+
 -- insert a few directory objects for testing
-INSERT INTO sys.utl_file_directory(dirname, dir)
-SELECT 'data_directory', current_setting('data_directory');
+insert into sys.utl_file_directory(dirname, dir)
+select 'data_directory', current_setting('data_directory');
 
--- test cases for UTL_FILE.FOPEN, UTL_FILE.FCLOSE, UTL_FILE.IS_OPEN
-DECLARE
-    ft sys.ORA_UTL_FILE_FILE_TYPE;
-    file_status BOOLEAN;
-BEGIN
-    -- a valid directory object name
-    ft := UTL_FILE.FOPEN('data_directory','test_file1.txt','a',2048);
-
-    IF ft.id IS NOT NULL THEN
-        RAISE NOTICE 'File opened successfully with ID: %, directory name: %, file name: %', ft.id, 'data_directory', 'test_file1.txt';
-    END IF;
-
-    SELECT UTL_FILE.IS_OPEN(ft) INTO STRICT file_status;
-    RAISE NOTICE 'Is file still open? file ID: % - (%)', ft.id, file_status;
-    UTL_FILE.FCLOSE(ft);
-    SELECT UTL_FILE.IS_OPEN(ft) INTO STRICT file_status;
-    RAISE NOTICE 'Is file still open? file ID: % - (%)', ft.id, file_status;
-END;
-/
-
--- test case for UTL_FILE.FOPEN with invalid directory object name
-DECLARE
-    ft sys.ORA_UTL_FILE_FILE_TYPE;
-BEGIN
+-- test case for fopen with invalid directory object name
+declare
+    f sys.ora_utl_file_file_type;
+begin
     -- an invalid directory object name
-    ft := UTL_FILE.FOPEN('RANDOM_DIR','test_file1.txt','a',2048);
-END;
+    f := utl_file.fopen('wrong_directory','regress.txt','a',1024);
+end;
 /
 
--- test case for UTL_FILE.FCLOSE_ALL
-DECLARE
-    ft1 sys.ORA_UTL_FILE_FILE_TYPE;
-    ft2 sys.ORA_UTL_FILE_FILE_TYPE;
-    ft3 sys.ORA_UTL_FILE_FILE_TYPE;
-    file_status1 BOOLEAN;
-    file_status2 BOOLEAN;
-    file_status3 BOOLEAN;
-BEGIN
-    ft1 := UTL_FILE.FOPEN('data_directory','test_file1.txt','a',2048);
-    ft2 := UTL_FILE.FOPEN('data_directory','test_file2.txt','a',2048);
-    ft3 := UTL_FILE.FOPEN('data_directory','test_file3.txt','a',2048);
+-- test fopen, put, putf, put_line, new_line, get_line, is_open, fclose, fclose_all
+declare
+    f sys.ora_utl_file_file_type;
+    line text;
+    i integer;
+begin
+    f := utl_file.fopen('data_directory', 'regress.txt', 'w');
 
-    SELECT UTL_FILE.IS_OPEN(ft1), UTL_FILE.IS_OPEN(ft2), UTL_FILE.IS_OPEN(ft3) INTO STRICT file_status1, file_status2, file_status3;
-    RAISE NOTICE 'Check if files are still open? file ID: % - (%), file ID: % - (%), file ID: % - (%)', ft1.id, file_status1, ft2.id, file_status2, ft3.id, file_status3;
-    UTL_FILE.FCLOSE_ALL();
-    SELECT UTL_FILE.IS_OPEN(ft1), UTL_FILE.IS_OPEN(ft2), UTL_FILE.IS_OPEN(ft3) INTO STRICT file_status1, file_status2, file_status3;
-    RAISE NOTICE 'Check if files are still open? file ID: % - (%), file ID: % - (%), file ID: % - (%)', ft1.id, file_status1, ft2.id, file_status2, ft3.id, file_status3;
-END;
+    -- data writing
+    utl_file.put_line(f, 'abc');
+    utl_file.put_line(f, '123'::numeric);
+    utl_file.put_line(f, '-----');
+    utl_file.new_line(f);
+    utl_file.put_line(f, '-----');
+    utl_file.new_line(f, 0);
+    utl_file.put_line(f, '-----');
+    utl_file.new_line(f, 2);
+    utl_file.put_line(f, '-----');
+    utl_file.put(f, 'a');
+    utl_file.put(f, 'b');
+    utl_file.new_line(f);
+    utl_file.putf(f, '[1=%s, 2=%s, 3=%s, 4=%s, 5=%s]', '1', '2', '3', '4', '5');
+    utl_file.new_line(f);
+    utl_file.put_line(f, '1234567890');
+    utl_file.fclose(f);
+
+    f := utl_file.fopen('data_directory', 'regress.txt', 'r');
+
+    -- data reading
+    i := 1;
+    utl_file.get_line(f, line);
+    while line is not null and i < 11 loop
+        raise notice '[%] >>%<<', i, line;
+        utl_file.get_line(f, line);
+        i:=i+1;
+
+    end loop;
+
+    while line is not null loop
+        raise notice '[%] >>%<<', i, line;
+        utl_file.get_line(f, line, 3);
+        i:=i+1;
+    end loop;
+
+    raise notice '>>%<<', line;
+    utl_file.fclose(f);
+
+    exception
+        when others then
+            raise notice 'exception: % ', sqlerrm;
+            raise notice 'is_open(f) = %', utl_file.is_open(f);
+            utl_file.fclose_all();
+            raise notice 'is_open(f) = %', utl_file.is_open(f);
+end;
 /
 
--- test cases for automatic file closing on session abnormally closing session or COMMIT/ROLLBACK/ABORT
-DECLARE
-    ft1 sys.ORA_UTL_FILE_FILE_TYPE;
-    ft2 sys.ORA_UTL_FILE_FILE_TYPE;
-    file_status1 BOOLEAN;
-    file_status2 BOOLEAN;
-BEGIN
-    ft1 := UTL_FILE.FOPEN('data_directory','test_file1.txt','a',2048);
-    ft2 := UTL_FILE.FOPEN('data_directory','test_file2.txt','a',2048);
-
-    SELECT UTL_FILE.IS_OPEN(ft1), UTL_FILE.IS_OPEN(ft2) INTO STRICT file_status1, file_status2;
-    RAISE NOTICE 'Check if files are still open? file ID: % - (%), file ID: % - (%)', ft1.id, file_status1, ft2.id, file_status2;
-    COMMIT;
-    SELECT UTL_FILE.IS_OPEN(ft1), UTL_FILE.IS_OPEN(ft2) INTO STRICT file_status1, file_status2;
-    RAISE NOTICE 'Check if files are still open? file ID: % - (%), file ID: % - (%)', ft1.id, file_status1, ft2.id, file_status2;
-END;
+-- test fgetattr, fcopy, fremove, frename
+declare
+    fexists boolean;
+    file_length integer;
+    block_size integer;
+begin
+    utl_file.fgetattr('data_directory', 'regress.txt', fexists, file_length, block_size);
+    raise notice 'fexists = %, file_length = %, block_size = %', fexists, file_length, block_size;
+    utl_file.fcopy('data_directory', 'regress.txt', 'data_directory', 'regress2.txt');
+    utl_file.fgetattr('data_directory', 'regress2.txt', fexists, file_length, block_size);
+    raise notice 'fexists = %, file_length = %, block_size = %', fexists, file_length, block_size;
+    utl_file.fremove('data_directory', 'regress2.txt');
+    utl_file.fgetattr('data_directory', 'regress2.txt', fexists, file_length, block_size);
+    raise notice 'fexists = %, file_length = %, block_size = %', fexists, file_length, block_size;
+    utl_file.frename('data_directory', 'regress.txt', 'data_directory', 'regress2.txt', true);
+    utl_file.fgetattr('data_directory', 'regress2.txt', fexists, file_length, block_size);
+    raise notice 'fexists = %, file_length = %, block_size = %', fexists, file_length, block_size;
+end;
 /
 
-DECLARE
-    ft1 sys.ORA_UTL_FILE_FILE_TYPE;
-    ft2 sys.ORA_UTL_FILE_FILE_TYPE;
-    file_status1 BOOLEAN;
-    file_status2 BOOLEAN;
-BEGIN
-    ft1 := UTL_FILE.FOPEN('data_directory','test_file1.txt','a',2048);
-    ft2 := UTL_FILE.FOPEN('data_directory','test_file2.txt','a',2048);
+-- test fflush, fseek
+declare
+    f sys.ora_utl_file_file_type;
+    f1 sys.ora_utl_file_file_type;
+    line text;
+    i integer;
+    line2_pos integer;
+begin
+    f := utl_file.fopen('data_directory', 'regressflush.txt', 'w');
+    f1 := utl_file.fopen('data_directory', 'regressflush.txt', 'r');
 
-    SELECT UTL_FILE.IS_OPEN(ft1), UTL_FILE.IS_OPEN(ft2) INTO STRICT file_status1, file_status2;
-    RAISE NOTICE 'Check if files are still open? file ID: % - (%), file ID: % - (%)', ft1.id, file_status1, ft2.id, file_status2;
-    ROLLBACK;
-    SELECT UTL_FILE.IS_OPEN(ft1), UTL_FILE.IS_OPEN(ft2) INTO STRICT file_status1, file_status2;
-    RAISE NOTICE 'Check if files are still open? file ID: % - (%), file ID: % - (%)', ft1.id, file_status1, ft2.id, file_status2;
+    -- data writing
+    for i in 1..5 loop
+        utl_file.put_line(f, 'line ' || i::text);
+    end loop;
 
-    RAISE NOTICE 'All files closed automatically by resource_cleanup_callback.';
-END;
+    -- data reading before fflush
+    raise notice 'data read before fflush';
+    utl_file.get_line(f1, line);
+    while line is not null loop
+        raise notice '>>%<<', line;
+        utl_file.get_line(f1, line);
+    end loop;
+    raise notice '>>%<<', line;
+
+    utl_file.fflush(f);
+
+    -- data reading after fflush
+    utl_file.fseek(f1); -- reset off_set to 0
+    raise notice 'data read after fflush';
+    utl_file.get_line(f1, line);
+    line2_pos := utl_file.fgetpos(f1); -- after reading line 1, get position
+    while line is not null loop
+        raise notice '>>%<<', line;
+        utl_file.get_line(f1, line);
+    end loop;
+    raise notice '>>%<<', line;
+    raise notice 'lets print line#2 again';
+    utl_file.fseek(f1, 0, line2_pos); -- seek to line 2 position
+    utl_file.get_line(f1, line);
+    raise notice '>>%<<', line;
+
+    utl_file.fclose_all();
+
+    exception
+        when others then
+            raise notice 'exception: % ', sqlerrm;
+            raise notice 'is_open(f) = %', utl_file.is_open(f);
+            raise notice 'is_open(f1) = %', utl_file.is_open(f1);
+            utl_file.fclose_all();
+            raise notice 'is_open(f) = %', utl_file.is_open(f);
+            raise notice 'is_open(f1) = %', utl_file.is_open(f1);
+end;
 /
 
-BEGIN;
-SELECT UTL_FILE.FOPEN('data_directory','test_file1.txt','a',2048);
-ABORT;
-/
+-- test cases for automatic file closing on session terminated/rollback etc
+declare
+    f sys.ora_utl_file_file_type;
+begin
+    f := utl_file.fopen('data_directory','regress.txt','a',1024);
 
--- test case for UTL_FILE.PUT_LINE
-DECLARE
-    ft sys.ORA_UTL_FILE_FILE_TYPE;
-    directory TEXT;
-    content TEXT;
-BEGIN
-    ft := UTL_FILE.FOPEN('data_directory','test_file1.txt','a',2048);
-
-    -- Write few lines to the file
-    UTL_FILE.PUT_LINE(ft,'line 1.', true);
-    UTL_FILE.PUT_LINE(ft,'line 2.', true);
-    UTL_FILE.PUT_LINE(ft,'line 3.', true);
-
-    -- Verify file content
-    SELECT current_setting('data_directory') INTO STRICT directory;
-    SELECT pg_read_file(directory || '/test_file1.txt') INTO STRICT content;
-
-    UTL_FILE.FCLOSE(ft);
-
-    RAISE INFO '%', content;
-END;
+    raise notice 'is_open = %', utl_file.is_open(f);
+    commit;
+    raise notice 'is_open = %', utl_file.is_open(f);
+    rollback;
+    raise notice 'is_open = %', utl_file.is_open(f);
+end;
 /
 
 -- clean up
-TRUNCATE sys.utl_file_directory;
+truncate sys.utl_file_directory;
+/
