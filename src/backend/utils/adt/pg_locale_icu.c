dba6165562383a26feead3660d44569c5f4b847d
Jeff Davis <jdavis@postgresql.org>
2026-01-29 10:37:09 -0800

    Fix memory leaks in pg_locale_icu.c.
    The backport prior to 18 requires minor modification due to code
    refactoring.
    
    Discussion: https://postgr.es/m/e2b7a0a88aaadded7e2d19f42d5ab03c9e182ad8.camel@j-davis.com
    Backpatch-through: 16

--- src/backend/utils/adt/pg_locale_icu.c
+++ src/backend/utils/adt/pg_locale_icu.c
@@ -597,6 +597,8 @@ make_icu_collator(const char *iculocstr, const char *icurules)
 							iculocstr, icurules, u_errorName(status))));
 		}
 
+		pfree(my_rules);
+		pfree(all_rules);
 		return collator_all_rules;
 	}
 }
@@ -1105,6 +1107,9 @@ strnxfrm_prefix_icu(char *dest, size_t destsize,
 		ereport(ERROR,
 				(errmsg("sort key generation failed: %s",
 						u_errorName(status))));
+
+	if (buf != sbuf)
+		pfree(buf);
 
 	return result_bsize;
 }
