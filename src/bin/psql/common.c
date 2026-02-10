5eb0d90d3f43e83186767dad2f4ec510a4597dc6
yasirmashwani <yasir.hussain.shah@gmail.com>
2026-02-10 22:53:39 +0500

    use INSTR_TIME_SET_ZERO instead of direct updating .ticks

--- src/bin/psql/common.c
+++ src/bin/psql/common.c
@@ -2860,7 +2860,7 @@ ExecQueryUsingCursor(const char *query, double *elapsed_msec)
 	int			flush_error;
 
 	*elapsed_msec = 0;
-	before.ticks = 0;
+	INSTR_TIME_SET_ZERO(before);
 
 	/* initialize print options for partial table output */
 	my_popt.topt.start_table = true;
