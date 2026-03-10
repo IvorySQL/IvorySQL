7de0b2a9ccb6001120b819e90756484ac64dff90
Álvaro Herrera <alvherre@kurilemu.de>
2026-03-10 19:56:39 +0100

    Introduce the REPACK command
    REPACK absorbs the functionality of VACUUM FULL and CLUSTER in a single
    command.  Because this functionality is completely different from
    regular VACUUM, having it separate from VACUUM makes it easier for users
    to understand; as for CLUSTER, the term is heavily overloaded in the
    IT world and even in Postgres itself, so it's good that we can avoid it.
    
    We retain those older commands, but de-emphasize them in the
    documentation, in favor of REPACK; the difference between VACUUM FULL
    and CLUSTER (namely, the fact that tuples are written in a specific
    ordering) is neatly absorbed as two different modes of REPACK.
    
    This allows us to introduce further functionality in the future that
    works regardless of whether an ordering is being applied, such as (and
    especially) a concurrent mode.
    
    Author: Antonin Houska <ah@cybertec.at>
    Reviewed-by: Mihail Nikalayeu <mihailnikalayeu@gmail.com>
    Reviewed-by: Álvaro Herrera <alvherre@kurilemu.de>
    Reviewed-by: Robert Treat <rob@xzilla.net>
    Reviewed-by: Euler Taveira <euler@eulerto.com>
    Reviewed-by: Matheus Alcantara <matheusssilv97@gmail.com>
    Reviewed-by: Junwang Zhao <zhjwpku@gmail.com>
    Reviewed-by: jian he <jian.universality@gmail.com>
    Discussion: https://postgr.es/m/82651.1720540558@antos
    Discussion: https://postgr.es/m/202507262156.sb455angijk6@alvherre.pgsql

--- src/backend/tcop/utility.c
+++ src/backend/tcop/utility.c
@@ -285,10 +285,10 @@ ClassifyUtilityCommandAsReadOnly(Node *parsetree)
 				return COMMAND_OK_IN_RECOVERY | COMMAND_OK_IN_READ_ONLY_TXN;
 			}
 
-		case T_ClusterStmt:
 		case T_ReindexStmt:
 		case T_OraAlterIndexRebuildStmt:	/* Oracle-compat ALTER INDEX ... REBUILD */
 		case T_VacuumStmt:
+		case T_RepackStmt:
 			{
 				/*
 				 * These commands write WAL, so they're not strictly
@@ -297,9 +297,9 @@ ClassifyUtilityCommandAsReadOnly(Node *parsetree)
 				 *
 				 * However, they don't change the database state in a way that
 				 * would affect pg_dump output, so it's fine to run them in a
-				 * read-only transaction. (CLUSTER might change the order of
-				 * rows on disk, which could affect the ordering of pg_dump
-				 * output, but that's not semantically significant.)
+				 * read-only transaction. (REPACK/CLUSTER might change the
+				 * order of rows on disk, which could affect the ordering of
+				 * pg_dump output, but that's not semantically significant.)
 				 */
 				return COMMAND_OK_IN_READ_ONLY_TXN;
 			}
@@ -863,12 +863,12 @@ standard_ProcessUtility(PlannedStmt *pstmt,
 			ExecuteCallStmt(castNode(CallStmt, parsetree), params, isAtomicContext, dest);
 			break;
 
-		case T_ClusterStmt:
-			cluster(pstate, (ClusterStmt *) parsetree, isTopLevel);
-			break;
-
 		case T_VacuumStmt:
 			ExecVacuum(pstate, (VacuumStmt *) parsetree, isTopLevel);
+			break;
+
+		case T_RepackStmt:
+			ExecRepack(pstate, (RepackStmt *) parsetree, isTopLevel);
 			break;
 
 		case T_ExplainStmt:
@@ -2922,15 +2922,18 @@ CreateCommandTag(Node *parsetree)
 			tag = CMDTAG_CALL;
 			break;
 
-		case T_ClusterStmt:
-			tag = CMDTAG_CLUSTER;
-			break;
-
 		case T_VacuumStmt:
 			if (((VacuumStmt *) parsetree)->is_vacuumcmd)
 				tag = CMDTAG_VACUUM;
 			else
 				tag = CMDTAG_ANALYZE;
+			break;
+
+		case T_RepackStmt:
+			if (((RepackStmt *) parsetree)->command == REPACK_COMMAND_CLUSTER)
+				tag = CMDTAG_CLUSTER;
+			else
+				tag = CMDTAG_REPACK;
 			break;
 
 		case T_ExplainStmt:
@@ -3597,7 +3600,7 @@ GetCommandLogLevel(Node *parsetree)
 			lev = LOGSTMT_ALL;
 			break;
 
-		case T_ClusterStmt:
+		case T_RepackStmt:
 			lev = LOGSTMT_DDL;
 			break;
 
