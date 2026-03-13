bfa85a3950261179afb3e4f71cd319f57afc4553
xiaoyu509 <3183378395@qq.com>
2026-03-13 16:25:17 +0800

    Delete inst directory
    Remove inst directory

--- inst/include/ecpg_config.h
+++
@@ -1,9 +0,0 @@
-/* src/interfaces/ecpg/include/ecpg_config.h.  Generated from ecpg_config.h.in by configure.  */
-/* Define to 1 to build client libraries as thread-safe code. */
-#define ENABLE_THREAD_SAFETY 1
-
-/* The size of `long', as computed by sizeof. */
-#define SIZEOF_LONG 8
-
-/* The size of `long long', as computed by sizeof. */
-#define SIZEOF_LONG_LONG 8
