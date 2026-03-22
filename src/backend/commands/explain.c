325f37982303af544e3261f3df84996f0697c2ec
Peter Geoghegan <pg@bowt.ie>
2026-03-22 13:20:29 -0400

    Make IndexScanInstrumentation a pointer in executor scan nodes.
    Change the IndexScanInstrumentation fields in IndexScanState,
    IndexOnlyScanState, and BitmapIndexScanState from inline structs to
    pointers.  This avoids additional space overhead whenever new fields are
    added to IndexScanInstrumentation in the future, at least in the common
    case where the instrumentation isn't used (i.e. when the executor node
    isn't being run through an EXPLAIN ANALYZE).
    
    Preparation for an upcoming patch series that will add index
    prefetching.  The new slot-based interface that will enable index
    prefetching necessitates that we add at least one more field to
    IndexScanInstrumentation (to count heap fetches during index-only
    scans).
    
    Author: Peter Geoghegan <pg@bowt.ie>
    Reviewed-By: Andres Freund <andres@anarazel.de>
    Discussion: https://postgr.es/m/CAH2-Wz=g=JTSyDB4UtB5su2ZcvsS7VbP+ZMvvaG6ABoCb+s8Lw@mail.gmail.com

--- src/backend/commands/explain.c
+++ src/backend/commands/explain.c
@@ -4029,7 +4029,7 @@ show_indexsearches_info(PlanState *planstate, ExplainState *es)
 			{
 				IndexScanState *indexstate = ((IndexScanState *) planstate);
 
-				nsearches = indexstate->iss_Instrument.nsearches;
+				nsearches = indexstate->iss_Instrument->nsearches;
 				SharedInfo = indexstate->iss_SharedInfo;
 				break;
 			}
@@ -4037,7 +4037,7 @@ show_indexsearches_info(PlanState *planstate, ExplainState *es)
 			{
 				IndexOnlyScanState *indexstate = ((IndexOnlyScanState *) planstate);
 
-				nsearches = indexstate->ioss_Instrument.nsearches;
+				nsearches = indexstate->ioss_Instrument->nsearches;
 				SharedInfo = indexstate->ioss_SharedInfo;
 				break;
 			}
@@ -4045,7 +4045,7 @@ show_indexsearches_info(PlanState *planstate, ExplainState *es)
 			{
 				BitmapIndexScanState *indexstate = ((BitmapIndexScanState *) planstate);
 
-				nsearches = indexstate->biss_Instrument.nsearches;
+				nsearches = indexstate->biss_Instrument->nsearches;
 				SharedInfo = indexstate->biss_SharedInfo;
 				break;
 			}
