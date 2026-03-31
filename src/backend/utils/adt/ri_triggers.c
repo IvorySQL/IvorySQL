b177587329b166fd0d490a890639a06a96a47b96
Nathan Bossart <nathan@postgresql.org>
2026-03-31 12:43:52 -0500

    Avoid including vacuum.h in tableam.h and heapam.h.
    Commit 2252fcd427 modified some function prototypes in tableam.h
    and heapam.h to take a VacuumParams argument instead of a pointer,
    which required including vacuum.h in those headers.  vacuum.h has a
    reasonably large dependency tree, and headers like tableam.h are
    widely included, so this is not ideal.  To fix, change the
    functions in question to accept a "const VacuumParams *" argument
    instead.  That allows us to use a forward declaration for
    VacuumParams and avoid including vacuum.h.  Since vacuum_rel()
    needs to scribble on the params argument, we still pass it by value
    to that function so that the original struct is not modified.
    
    Reported-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Andres Freund <andres@anarazel.de>
    Reviewed-by: Michael Paquier <michael@paquier.xyz>
    Discussion: https://postgr.es/m/rzxpxod4c4la62yvutyrvgoyilrl2fx55djaf2suidy7np5m6c%403l2ln476eadh

--- src/backend/utils/adt/ri_triggers.c
+++ src/backend/utils/adt/ri_triggers.c
@@ -24,6 +24,7 @@
 
 #include "postgres.h"
 
+#include "access/genam.h"
 #include "access/htup_details.h"
 #include "access/skey.h"
 #include "access/sysattr.h"
@@ -47,6 +48,7 @@
 #include "utils/datum.h"
 #include "utils/fmgroids.h"
 #include "utils/guc.h"
+#include "utils/hsearch.h"
 #include "utils/inval.h"
 #include "utils/lsyscache.h"
 #include "utils/memutils.h"
