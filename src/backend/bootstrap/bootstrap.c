7fe30fd6d866c5c553e57d13ac08c4dc0248dd16
Heikki Linnakangas <heikki.linnakangas@iki.fi>
2026-04-06 02:12:50 +0300

    Introduce a new mechanism for registering shared memory areas
    This replaces the [Subsystem]ShmemSize() and [Subsystem]ShmemInit()
    functions called at postmaster startup with a new set of callbacks.
    The new mechanism is designed to be more ergonomic. Notably, the size
    of each shmem area is specified in the same ShmemRequestStruct() call,
    together with its name. The same mechanism is used in extensions,
    replacing the shmem_{request/startup}_hooks.
    
    ShmemInitStruct() and ShmemInitHash() become backwards-compatibility
    wrappers around the new functions. In future commits, I will replace
    all ShmemInitStruct() and ShmemInitHash() calls with the new
    functions, although we'll still need to keep them around for
    extensions.
    
    Co-authored-by: Ashutosh Bapat <ashutosh.bapat.oss@gmail.com>
    Reviewed-by: Matthias van de Meent <boekewurm+postgres@gmail.com>
    Reviewed-by: Zsolt Parragi <zsolt.parragi@percona.com>
    Reviewed-by: Robert Haas <robertmhaas@gmail.com>
    Reviewed-by: Daniel Gustafsson <daniel@yesql.se>
    Discussion: https://www.postgresql.org/message-id/CAExHW5vM1bneLYfg0wGeAa=52UiJ3z4vKd3AJ72X8Fw6k3KKrg@mail.gmail.com

--- src/backend/bootstrap/bootstrap.c
+++ src/backend/bootstrap/bootstrap.c
@@ -40,6 +40,7 @@
 #include "storage/fd.h"
 #include "storage/ipc.h"
 #include "storage/proc.h"
+#include "storage/shmem_internal.h"
 #include "utils/builtins.h"
 #include "utils/fmgroids.h"
 #include "utils/guc.h"
@@ -392,6 +393,7 @@ BootstrapModeMain(int argc, char *argv[], bool check_only)
 
 	InitializeFastPathLocks();
 
+	ShmemCallRequestCallbacks();
 	CreateSharedMemoryAndSemaphores();
 
 	/*
