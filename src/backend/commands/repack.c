2a1b9b572da21a322a2151919af7a65fb6eb1bd3
Álvaro Herrera <alvherre@kurilemu.de>
2026-04-08 22:58:56 +0200

    Simplify declaration of memcpy target
    The existing one is understandable failing on (some?) 32-bit platforms.
    
    Reported-by: Tomas Vondra <tomas@vondra.me>
    Suggested-by: Tom Lane <tgl@sss.pgh.pa.us>
    Discussion: https://postgr.es/m/1c197f2d-49a2-4830-8dde-55867218b62d@vondra.me

--- src/backend/commands/repack.c
+++ src/backend/commands/repack.c
@@ -2726,11 +2726,7 @@ restore_tuple(BufFile *file, Relation relation, TupleTableSlot *slot)
 		{
 			CompactAttribute *attr = TupleDescCompactAttr(desc, i);
 			varlena    *varlen;
-			union
-			{
-				alignas(int32) varlena hdr;
-				char		data[sizeof(void *)];
-			}			chunk_header;
+			uint64		chunk_header;
 			void	   *value;
 			Size		varlensz;
 
