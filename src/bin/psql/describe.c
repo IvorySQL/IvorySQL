21f5b6b2a00c5606a23b73b10d0ab8a469b354d9
Amit Kapila <akapila@postgresql.org>
2026-04-13 10:54:16 +0530

    Fix capitalization in publication describe output.
    Consistent with existing psql metadata display conventions, update the
    description tags for EXCEPT publications to use lowercase for the second
    word (e.g., "Except tables" instead of "Except Tables"). This aligns the
    output style with other publication describe commands.
    
    Author: Peter Smith <smithpb2250@gmail.com>
    Reviewed-by: vignesh C <vignesh21@gmail.com>
    Discussion: https://postgr.es/m/CAHut+Pt3t_tCYwDStkj5fG4Z=YXrHvPBA7iGdh745QipC5zKeg@mail.gmail.com

--- src/bin/psql/describe.c
+++ src/bin/psql/describe.c
@@ -3365,7 +3365,7 @@ describeOneTableDetails(const char *schemaname,
 				tuples = PQntuples(result);
 
 			if (tuples > 0)
-				printTableAddFooter(&cont, _("Except Publications:"));
+				printTableAddFooter(&cont, _("Except publications:"));
 
 			/* Might be an empty set - that's ok */
 			for (i = 0; i < tuples; i++)
