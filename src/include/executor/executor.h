12565571791fb5999f1a98507eb8f0340191a775
Peter Eisentraut <peter@eisentraut.org>
2026-06-27 19:34:40 +0200

    Reject child partition FDWs in FOR PORTION OF
    We should defer validating FDW usage until after analysis.  We have to
    guard against not just the topmost table, but also individual child
    partitions.  Added the check to CheckValidResultRel, because it is
    called after looking up child partitions (accounting for pruning), but
    before the FDW can run a DirectModify update, which would bypass
    per-tuple executor work.
    
    Author: jian he <jian.universality@gmail.com>
    Reported-by: Tom Lane <tgl@sss.pgh.pa.us>
    Reviewed-by: Paul A. Jungwirth <pj@illuminatedcomputing.com>
    Discussion: https://www.postgresql.org/message-id/flat/CA%2BrenyUte0_UJsJiDJQi82oaBsMJn%3Dcct0Wn%3DvOqXtuDn%3DYYJA%40mail.gmail.com

--- src/include/executor/executor.h
+++ src/include/executor/executor.h
@@ -260,7 +260,7 @@ extern bool ExecCheckPermissions(List *rangeTable,
 extern bool ExecCheckOneRelPerms(RTEPermissionInfo *perminfo);
 extern void CheckValidResultRel(ResultRelInfo *resultRelInfo, CmdType operation,
 								OnConflictAction onConflictAction,
-								List *mergeActions);
+								List *mergeActions, ModifyTable *mtnode);
 extern void InitResultRelInfo(ResultRelInfo *resultRelInfo,
 							  Relation resultRelationDesc,
 							  Index resultRelationIndex,
