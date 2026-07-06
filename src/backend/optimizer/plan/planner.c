30432775dea2d777c4d8d7740b0c66bca11a5575
Peter Eisentraut <peter@eisentraut.org>
2026-07-06 09:19:02 +0200

    Forbid generated columns in FOR PORTION OF
    With virtual generated columns there is no column to assign to, and we
    shouldn't assign directly to stored generated columns either.  (Once
    we have PERIODs, we will allow a stored generated column here, but we
    will assign to its start/end inputs.)
    
    We can't do this in parse analysis, because views haven't yet been
    rewritten, so they mask generated columns.
    
    Author: Paul A. Jungwirth <pj@illuminatedcomputing.com>
    Discussion: https://www.postgresql.org/message-id/agOOykf2HV26yVfU%40nathan

--- src/backend/optimizer/plan/planner.c
+++ src/backend/optimizer/plan/planner.c
@@ -1274,6 +1274,32 @@ subquery_planner(PlannerGlobal *glob, Query *parse, char *plan_name,
 	transform_MERGE_to_join(parse);
 
 	/*
+	 * Reject FOR PORTION OF on a generated column.  We can't write to a
+	 * virtual generated column, and a stored generated column should be
+	 * written by its own expression.
+	 *
+	 * We do this in the planner rather than parse analysis so that updatable
+	 * views have been rewritten; otherwise they would mask which columns are
+	 * generated.  We need to check before preprocess_relation_rtes(), so that
+	 * for virtual generated columns we still have the rangeVar.  After that
+	 * it is replaced by the column's expression.
+	 *
+	 * XXX: We plan to implement PERIODs as stored generated columns, so later
+	 * we will loosen this restriction if the column belongs to a PERIOD.
+	 */
+	if (parse->forPortionOf)
+	{
+		ForPortionOfExpr *forPortionOf = parse->forPortionOf;
+		RangeTblEntry *rte = rt_fetch(parse->resultRelation, parse->rtable);
+
+		if (get_attgenerated(rte->relid, forPortionOf->rangeVar->varattno))
+			ereport(ERROR,
+					(errcode(ERRCODE_FEATURE_NOT_SUPPORTED),
+					 errmsg("cannot use generated column \"%s\" in FOR PORTION OF",
+							forPortionOf->range_name)));
+	}
+
+	/*
 	 * Scan the rangetable for relation RTEs and retrieve the necessary
 	 * catalog information for each relation.  Using this information, clear
 	 * the inh flag for any relation that has no children, collect not-null
