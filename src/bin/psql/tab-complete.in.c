c59c2e5b0c8348f5ac7de0dbfb748241b7cf7adc
Peter Eisentraut <peter@eisentraut.org>
2026-07-06 11:44:55 +0200

    Remove apparent support for SECURITY LABEL ON PROPERTY GRAPH
    Commit 2f094e7ac69 added a mention of SECURITY LABEL ON PROPERTY GRAPH
    to the SECURITY LABEL reference page, and it added support to psql tab
    completion.  However, security labels on property graphs are not
    actually supported (per SecLabelSupportsObjectType()).  The syntax
    does work, but that is just a result of how gram.y is factored.  We
    don't document or tab-complete the syntax of SECURITY LABEL for other
    object types that are not actually supported, so it was inconsistent
    to do this for property graphs.  Thus, remove this.
    
    Reported-by: Noah Misch <noah@leadboat.com>
    Discussion: https://www.postgresql.org/message-id/flat/20260704221210.08.noahmisch%40microsoft.com

--- src/bin/psql/tab-complete.in.c
+++ src/bin/psql/tab-complete.in.c
@@ -5289,10 +5289,8 @@ match_previous_words(int pattern_id,
 		COMPLETE_WITH("TABLE", "COLUMN", "AGGREGATE", "DATABASE", "DOMAIN",
 					  "EVENT TRIGGER", "FOREIGN TABLE", "FUNCTION",
 					  "LARGE OBJECT", "MATERIALIZED VIEW", "LANGUAGE",
-					  "PROPERTY GRAPH", "PUBLICATION", "PROCEDURE", "ROLE", "ROUTINE", "SCHEMA",
+					  "PUBLICATION", "PROCEDURE", "ROLE", "ROUTINE", "SCHEMA",
 					  "SEQUENCE", "SUBSCRIPTION", "TABLESPACE", "TYPE", "VIEW");
-	else if (Matches("SECURITY", "LABEL", "ON", "PROPERTY", "GRAPH"))
-		COMPLETE_WITH_SCHEMA_QUERY(Query_for_list_of_propgraphs);
 	else if (Matches("SECURITY", "LABEL", "ON", MatchAny, MatchAny))
 		COMPLETE_WITH("IS");
 
