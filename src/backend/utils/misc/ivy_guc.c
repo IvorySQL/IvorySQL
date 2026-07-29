b7604c5122ea07563ef8034bff6ae13a1f114931
gaoxueyu <44825804+gaoxueyu@users.noreply.github.com>
2026-07-29 17:25:06 +0800

    Merge pull request #1450 from enkilee/fix-ivy_guc-bug
    disallow setting ivorysql.enable_case_switch via ALTER USER/DATABASE SET
    
    (cherry picked from commit a83c5d98733699572178b0425f8fc7c933cbd439)

--- src/backend/utils/misc/ivy_guc.c
+++ src/backend/utils/misc/ivy_guc.c
@@ -128,7 +128,7 @@ static struct config_bool Ivy_ConfigureNamesBool[] =
 		{"ivorysql.enable_case_switch", PGC_USERSET, CLIENT_CONN_STATEMENT,
 			gettext_noop("whether enable case conversion feature in oracle compatible mode."),
 			NULL,
-			GUC_NO_SHOW_ALL | GUC_NOT_IN_SAMPLE | GUC_DISALLOW_IN_FILE
+			GUC_NOT_IN_SAMPLE
 		},
 		&enable_case_switch,
 		true,
