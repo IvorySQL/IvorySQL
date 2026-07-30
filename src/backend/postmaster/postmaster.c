910f0c4ee1e7803ac4f5ff58a9ded5a5455dd0ea
liuxh-Desktop-U24 <liuxh.zj.cn@gmail.com>
2026-07-30 08:49:13 +0800

    Replace getopt() with our re-entrant variant in the backend
    Some of these probably could continue using non-re-entrant getopt()
    even if we start using threads in the future, but it seems better to
    make them all anyway, so that we have a clear-cut rule of "no plain
    getopt() in the postgres binary".
    
    Reviewed-by: Peter Eisentraut <peter@eisentraut.org>
    Discussion: https://www.postgresql.org/message-id/d1da5f0e-0d68-47c9-a882-eb22f462752f@iki.fi

--- src/backend/postmaster/postmaster.c
+++ src/backend/postmaster/postmaster.c
@@ -98,10 +98,10 @@
 #include "lib/ilist.h"
 #include "libpq/libpq.h"
 #include "libpq/pqsignal.h"
-#include "pg_getopt.h"
 #include "pgstat.h"
 #include "parser/scansup.h"
 #include "port/pg_bswap.h"
+#include "port/pg_getopt_ctx.h"
 #include "postmaster/autovacuum.h"
 #include "postmaster/bgworker_internals.h"
 #include "postmaster/pgarch.h"
@@ -502,6 +502,7 @@ HANDLE		PostmasterHandle;
 void
 PostmasterMain(int argc, char *argv[])
 {
+	pg_getopt_ctx optctx;
 	int			opt;
 	int			status;
 	char	   *userDoption = NULL;
@@ -598,19 +599,19 @@ PostmasterMain(int argc, char *argv[])
 	 */
 	InitializeGUCOptions();
 
-	opterr = 1;
-
 	/*
 	 * Parse command-line options.  CAUTION: keep this in sync with
 	 * tcop/postgres.c (the option sets should not conflict) and with the
 	 * common help() function in main/main.c.
 	 */
-	while ((opt = getopt(argc, argv, "B:bc:C:D:d:EeFf:h:ijk:lN:nOo:Pp:r:S:sTt:W:-:")) != -1)
+	pg_getopt_start(&optctx, argc, argv, "B:bc:C:D:d:EeFf:h:ijk:lN:nOo:Pp:r:S:sTt:W:-:");
+	optctx.opterr = 1;
+	while ((opt = pg_getopt_next(&optctx)) != -1)
 	{
 		switch (opt)
 		{
 			case 'B':
-				SetConfigOption("shared_buffers", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("shared_buffers", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 'b':
@@ -619,7 +620,7 @@ PostmasterMain(int argc, char *argv[])
 				break;
 
 			case 'C':
-				output_config_variable = strdup(optarg);
+				output_config_variable = strdup(optctx.optarg);
 				break;
 
 			case '-':
@@ -630,10 +631,10 @@ PostmasterMain(int argc, char *argv[])
 				 * returns DISPATCH_POSTMASTER if it doesn't find a match, so
 				 * error for anything else.
 				 */
-				if (parse_dispatch_option(optarg) != DISPATCH_POSTMASTER)
+				if (parse_dispatch_option(optctx.optarg) != DISPATCH_POSTMASTER)
 					ereport(ERROR,
 							(errcode(ERRCODE_SYNTAX_ERROR),
-							 errmsg("--%s must be first argument", optarg)));
+							 errmsg("--%s must be first argument", optctx.optarg)));
 
 				pg_fallthrough;
 			case 'c':
@@ -641,19 +642,19 @@ PostmasterMain(int argc, char *argv[])
 					char	   *name,
 							   *value;
 
-					ParseLongOption(optarg, &name, &value);
+					ParseLongOption(optctx.optarg, &name, &value);
 					if (!value)
 					{
 						if (opt == '-')
 							ereport(ERROR,
 									(errcode(ERRCODE_SYNTAX_ERROR),
 									 errmsg("--%s requires a value",
-											optarg)));
+											optctx.optarg)));
 						else
 							ereport(ERROR,
 									(errcode(ERRCODE_SYNTAX_ERROR),
 									 errmsg("-c %s requires a value",
-											optarg)));
+											optctx.optarg)));
 					}
 
 					SetConfigOption(name, value, PGC_POSTMASTER, PGC_S_ARGV);
@@ -663,11 +664,11 @@ PostmasterMain(int argc, char *argv[])
 				}
 
 			case 'D':
-				userDoption = strdup(optarg);
+				userDoption = strdup(optctx.optarg);
 				break;
 
 			case 'd':
-				set_debug_options(atoi(optarg), PGC_POSTMASTER, PGC_S_ARGV);
+				set_debug_options(atoi(optctx.optarg), PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 'E':
@@ -683,16 +684,16 @@ PostmasterMain(int argc, char *argv[])
 				break;
 
 			case 'f':
-				if (!set_plan_disabling_options(optarg, PGC_POSTMASTER, PGC_S_ARGV))
+				if (!set_plan_disabling_options(optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV))
 				{
 					write_stderr("%s: invalid argument for option -f: \"%s\"\n",
-								 progname, optarg);
+								 progname, optctx.optarg);
 					ExitPostmaster(1);
 				}
 				break;
 
 			case 'h':
-				SetConfigOption("listen_addresses", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("listen_addresses", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 'i':
@@ -704,7 +705,7 @@ PostmasterMain(int argc, char *argv[])
 				break;
 
 			case 'k':
-				SetConfigOption("unix_socket_directories", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("unix_socket_directories", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 'l':
@@ -712,7 +713,7 @@ PostmasterMain(int argc, char *argv[])
 				break;
 
 			case 'N':
-				SetConfigOption("max_connections", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("max_connections", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 'O':
@@ -720,14 +721,14 @@ PostmasterMain(int argc, char *argv[])
 				break;
 
 			case 'o':
-				SetConfigOption("ivorysql.port", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("ivorysql.port", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 			case 'P':
 				SetConfigOption("ignore_system_indexes", "true", PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 'p':
-				SetConfigOption("port", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("port", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 'r':
@@ -735,7 +736,7 @@ PostmasterMain(int argc, char *argv[])
 				break;
 
 			case 'S':
-				SetConfigOption("work_mem", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("work_mem", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			case 's':
@@ -753,7 +754,7 @@ PostmasterMain(int argc, char *argv[])
 
 			case 't':
 				{
-					const char *tmp = get_stats_option_name(optarg);
+					const char *tmp = get_stats_option_name(optctx.optarg);
 
 					if (tmp)
 					{
@@ -762,14 +763,14 @@ PostmasterMain(int argc, char *argv[])
 					else
 					{
 						write_stderr("%s: invalid argument for option -t: \"%s\"\n",
-									 progname, optarg);
+									 progname, optctx.optarg);
 						ExitPostmaster(1);
 					}
 					break;
 				}
 
 			case 'W':
-				SetConfigOption("post_auth_delay", optarg, PGC_POSTMASTER, PGC_S_ARGV);
+				SetConfigOption("post_auth_delay", optctx.optarg, PGC_POSTMASTER, PGC_S_ARGV);
 				break;
 
 			default:
@@ -782,10 +783,10 @@ PostmasterMain(int argc, char *argv[])
 	/*
 	 * Postmaster accepts no non-option switch arguments.
 	 */
-	if (optind < argc)
+	if (optctx.optind < argc)
 	{
 		write_stderr("%s: invalid argument: \"%s\"\n",
-					 progname, argv[optind]);
+					 progname, argv[optctx.optind]);
 		write_stderr("Try \"%s --help\" for more information.\n",
 					 progname);
 		ExitPostmaster(1);
@@ -883,15 +884,6 @@ PostmasterMain(int argc, char *argv[])
 		write_stderr("%s: invalid datetoken tables, please fix\n", progname);
 		ExitPostmaster(1);
 	}
-
-	/*
-	 * Now that we are done processing the postmaster arguments, reset
-	 * getopt(3) library so that it will work correctly in subprocesses.
-	 */
-	optind = 1;
-#ifdef HAVE_INT_OPTRESET
-	optreset = 1;				/* some systems need this too */
-#endif
 
 	/* For debugging: display postmaster environment */
 	if (message_level_is_interesting(DEBUG3))
